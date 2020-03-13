1861f5b3b1d17c7c9642e8bea512daba7ecfe812
Alexis Grow <agrow@yext.com>
2020-03-13 13:20:04 -0400

    add card dataForRender mappings and remove other mapping capabilities

--- cards/standard/card_component.js
+++ cards/standard/card_component.js
@@ -1,63 +1,38 @@
 const template = `{{{read 'cards_standard_card_template' }}}`;
-
-class StandardCardConfig {
-  constructor(config = {}) {
-    Object.assign(this, config);
-
-    const result = config.data.result || {};
-
-    const rawResult = result._raw || {};
-
-    let cardMappings = config.cardMappings || {};
-    Object.assign(this, this.applyCardMappings(result, cardMappings));
-
-    this.title = this.title || result.title || rawResult.name || '';
-
-    this.details = this.details || result.details || rawResult.description || '';
-
-    this.url = this.url || result.link || rawResult.website;
-
-    this.subtitle = this.subtitle;
-
-    this.callsToAction = this.callsToAction || [];
-
-    this.callsToActionFields = this.callsToActionFields || [];
-  }
-
-  /**
-   * Used to apply given card mappings from config.
-   * @param {Result} result
-   * @param {Object|Function} cardMappings
-   */
-  applyCardMappings(result, cardMappings) {
-    const config = {};
-    if (typeof cardMappings === 'function') {
-      cardMappings = cardMappings(result);
-    }
-    if (typeof cardMappings === 'object') {
-      Object.entries(cardMappings).forEach(([attribute, value]) => {
-        if (typeof value === 'function') {
-          config[attribute] = value(result);
-        } else {
-          config[attribute] = value;
-        }
-      });
-    }
-    return config;
-  }
-}
-
-
 class StandardCardComponent extends ANSWERS.Component {
-  constructor(config = {}, systemConfig = {}) {
-    super(new StandardCardConfig(config), systemConfig);
+  /**
+   * This returns an object that will be called `card`
+   * in the template. Put all mapping logic here.
+   *
+   * @param profile profile of the entity in the card
+   */
+  dataForRender(profile) {
+    let title = profile.name;
+    let subtitle = '';
+    let details = profile.description;
+    let titleUrl = profile.websites;
+    let urlShouldOpenInNewWindow = true;
 
+    return {
+      title: title,
+      titleUrl: titleUrl,
+      newWindow: urlShouldOpenInNewWindow,
+      subtitle: subtitle,
+      details: details
+    }
+  }
+
+  /***********************************************
+   *
+   * Do not make changes below this point
+   *
+   **********************************************/
+  constructor(config = {}, systemConfig = {}) {
+    super(config, systemConfig);
     const data = config.data || {};
 
     this.verticalKey = data.verticalKey;
-
     this.result = data.result || {};
-
     this.setTemplate(template);
   }
 
@@ -65,6 +40,7 @@ class StandardCardComponent extends ANSWERS.Component {
     const details = this._config.details;
     return super.setState({
       ...data,
+      card: this.dataForRender(this.result._raw),
       result: this.result,
       details
     });
