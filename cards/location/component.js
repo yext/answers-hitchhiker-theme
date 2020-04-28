81b3b9ee2a00b5922936f9fe12fe8bf8ad507118
Alexis Grow <agrow@yext.com>
2020-04-28 18:54:43 -0400

    pr fixes

--- cards/location/component.js
+++ cards/location/component.js
@@ -32,7 +32,7 @@ class LocationCardComponent extends BaseCard.LocationCard {
       CTA1: { // The primary call to action for the card
         iconName: 'phone', // The icon to use for the CTA
         label: 'Call', // The label of the CTA
-        url: HitchhikerJS.Formatters.phoneLink(profile) , // The URL a user will be directed to when clicking
+        url: HitchhikerJS.Formatters.phoneLink(profile), // The URL a user will be directed to when clicking
         target: '_top', // If the URL will be opened in a new tab, etc.
         eventType: 'TAP_TO_CALL', // Type of Analytics event fired when clicking the CTA
         eventOptions: this.addDefaultEventOptions(), // The analytics event options for CTA clicks
