2f1339b84aca26ae1d72f57d880857d7da5a3949
Alexis Grow <39104038+alexisgrow@users.noreply.github.com>
2020-04-29 17:14:36 -0400

    Add Link Card (#68)
    Add a new card type to the HH theme. The Link Card has a title, subtitle, and description.
    
    TEST=manual
    
    Use the Link Card on a Vertical and Universal page. Test on mobile, tablet, and desktop. Hardcode data in dataForRender to test no data and long data cases.

---
+++ cards/link/component.js
@@ -0,0 +1,34 @@
+{{> cards/card_component componentName='LinkCard'}}
+
+class LinkCardComponent extends BaseCard.LinkCard {
+  constructor(config = {}, systemConfig = {}) {
+    super(config, systemConfig);
+    this.setTemplate(`{{{read 'cards/link/template' }}}`);
+  }
+
+  /**
+   * This returns an object that will be called `card`
+   * in the template. Put all mapping logic here.
+   *
+   * @param profile profile of the entity in the card
+   */
+  dataForRender(profile) {
+    return {
+      title: profile.htmlTitle, // The header text of the card
+      url: profile.link, // If the card title is a clickable link, set URL here
+      target: '_top', // If the title's URL should open in a new tab, etc.
+      titleEventOptions: this.addDefaultEventOptions(),
+      // subtitle: '', // The sub-header text of the card
+      // If the card's details are longer than a certain character count, you can truncate the
+      // text. A toggle will be supplied that can show or hide the truncated text.
+      // showMoreDetails: {
+      //   showMoreLimit: 750, // Character count limit
+      //   showMoreText: 'Show more', // Label when toggle will show truncated text
+      //   showLessText: 'Show less' // Label when toggle will hide truncated text
+      // },
+      details: profile.htmlSnippet, // The text in the body of the card
+    };
+  }
+}
+
+ANSWERS.registerComponentType(LinkCardComponent);
