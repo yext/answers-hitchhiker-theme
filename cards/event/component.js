18c2303892ec6119425588667995abd6a7141398
Alexis Grow <agrow@yext.com>
2020-04-28 22:40:46 -0400

    Add Event Card

---
+++ cards/event/component.js
@@ -0,0 +1,53 @@
+{{> cards/card_component componentName='EventCard'}}
+
+class EventCardComponent extends BaseCard.EventCard {
+  constructor(config = {}, systemConfig = {}) {
+    super(config, systemConfig);
+    this.setTemplate(`{{{read 'cards/event/template' }}}`);
+  }
+
+  /**
+   * This returns an object that will be called `card`
+   * in the template. Put all mapping logic here.
+   *
+   * @param profile profile of the entity in the card
+   */
+  dataForRender(profile) {
+    return {
+      title: profile.name, // The header text of the card
+      url: profile.website, // If the card title is a clickable link, set URL here
+      target: '_top', // If the title's URL should open in a new tab, etc.
+      titleEventOptions: this.addDefaultEventOptions(),
+      date: HitchhikerJS.Formatters.bigDate(profile),
+      subtitle: HitchhikerJS.Formatters.dateRange(profile),
+      details: profile.description, // The text in the body of the card
+      // If the card's details are longer than a certain character count, you can truncate the
+      // text. A toggle will be supplied that can show or hide the truncated text.
+      // showMoreDetails: {
+      //   showMoreLimit: 750, // Character count limit
+      //   showMoreText: 'Show more', // Label when toggle will show truncated text
+      //   showLessText: 'Show less' // Label when toggle will hide truncated text
+      // },
+      // The primary CTA of the card
+      CTA1: {
+        label: 'RSVP', // The CTA's label
+        iconName: 'calendar', // The icon to use for the CTA
+        url: profile.ticketUrl || profile.website, // The URL a user will be directed to when clicking
+        target: '_top', // Where the new URL will be opened
+        eventType: 'RSVP', // Type of Analytics event fired when clicking the CTA
+        eventOptions: this.addDefaultEventOptions()
+      },
+      // The secondary CTA of the card
+      CTA2: {
+        label: 'Directions',
+        iconName: 'directions',
+        url: HitchhikerJS.Formatters.getDirectionsUrl(profile),
+        target: '_top',
+        eventType: 'DRIVING_DIRECTIONS',
+        eventOptions: this.addDefaultEventOptions()
+      }
+    };
+  }
+}
+
+ANSWERS.registerComponentType(EventCardComponent);
