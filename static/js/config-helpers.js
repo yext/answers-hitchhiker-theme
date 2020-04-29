5ef579740652a898329487b744d0b48fe92ff964
Oliver Shi <oshi@yext.com>
2020-04-29 11:39:14 -0400

    Use nintendo switch

--- static/js/config-helpers.js
+++ static/js/config-helpers.js
@@ -2,12 +2,10 @@
  * Contains some helpers for adding SDK configuration.
  */
 export function getDefaultMapApiKey(mapProvider) {
-    const _mapProvider = mapProvider.toLowerCase();
-    let defaultMapApiKey = '';
-    if (_mapProvider === 'google') {
-      defaultMapApiKey = 'AIzaSyB5D45ghF1YMfqTLSzWubmlCN1euBVPhFw';
-    } else if (_mapProvider === 'mapbox') {
-      defaultMapApiKey = 'pk.eyJ1IjoieWV4dCIsImEiOiJqNzVybUhnIn0.hTOO5A1yqfpN42-_z_GuLw';
+    switch (mapProvider.toLowerCase()) {
+      case 'google':
+        return 'AIzaSyB5D45ghF1YMfqTLSzWubmlCN1euBVPhFw'
+      case 'mapbox':
+        return 'pk.eyJ1IjoieWV4dCIsImEiOiJqNzVybUhnIn0.hTOO5A1yqfpN42-_z_GuLw'
     }
-    return defaultMapApiKey;
 }
