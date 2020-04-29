60e2cb425305733d4f6ead71eeca258ee8727e84
Alexis Grow <agrow@yext.com>
2020-04-29 14:33:34 -0400

    Add Link Card

---
+++ cards/link/component.js
@@ -0,0 +1,29 @@
+{{> cards/card_component componentName='LinkCard'}}
+
+class LinkCardComponent extends BaseCard.LinkCard {
+  constructor(config = {}, systemConfig = {}) {
+    super(config, systemConfig);
+    this.setTemplate(`{{{read 'cards/link/template' }}}`);
+  }
+
+  /**
+   * This returns an object that will be called `card`
+   * in the template. Put all mapping logic here.
+   *
+   * @param profile profile of the entity in the card
+   */
+  dataForRender(profile) {
+    return {
+      title: profile.htmlTitle, // The header text of the card
+      url: profile.link, // If the card title is a clickable link, set URL here
+      target: '_top', // If the title's URL should open in a new tab, etc.
+      // image: '', // The URL of the image to display on the card
+      // tagLabel: '', // The label of the displayed image
+      titleEventOptions: this.addDefaultEventOptions(),
+      // subtitle: '', // The sub-header text of the card
+      details: profile.htmlSnippet, // The text in the body of the card
+    };
+  }
+}
+
+ANSWERS.registerComponentType(LinkCardComponent);
