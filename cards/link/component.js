013c605b6cbc3fd72a30e66f6a3a2e6d458cbb29
Alexis Grow <agrow@yext.com>
2020-04-29 15:55:43 -0400

    add showMoreDetails comment block

--- cards/link/component.js
+++ cards/link/component.js
@@ -19,6 +19,13 @@ class LinkCardComponent extends BaseCard.LinkCard {
       target: '_top', // If the title's URL should open in a new tab, etc.
       titleEventOptions: this.addDefaultEventOptions(),
       // subtitle: '', // The sub-header text of the card
+      // If the card's details are longer than a certain character count, you can truncate the
+      // text. A toggle will be supplied that can show or hide the truncated text.
+      // showMoreDetails: {
+      //   showMoreLimit: 750, // Character count limit
+      //   showMoreText: 'Show more', // Label when toggle will show truncated text
+      //   showLessText: 'Show less' // Label when toggle will hide truncated text
+      // },
       details: profile.htmlSnippet, // The text in the body of the card
     };
   }
