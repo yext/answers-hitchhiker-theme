1919c942b46071e1e0b66424f5019eb95fe0c840
Oliver Shi <oshi@yext.com>
2020-05-05 02:38:12 -0400

    remove unused comments

--- static/entry.js
+++ static/entry.js
@@ -6,14 +6,3 @@ import Formatters from './js/formatters';
 export { Formatters };
 export { getDefaultMapApiKey } from './js/default-map-api-key';
 export { isStaging } from './js/is-staging';
-
-// TODO seems not great to hardcode the location of the jambo config
-// import JamboConfig from '../jambo.json';
-
-// import km from '../desktop/km.html';
-// Import all jambo build html files
-// function importAll (r) {
-//   r.keys().forEach(r);
-// }
-// const jamboHtml = require.context(`../${JamboConfig.dirs.output}`, true, /\.html$/i);
-// importAll(jamboHtml);
