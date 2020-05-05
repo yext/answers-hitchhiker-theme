836701d7d24c695cfce8490cc0b811d25642e364
Oliver Shi <oshi@yext.com>
2020-05-05 12:48:33 -0400

    jambo build must happen before webpack

--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
 	  watch: {
 		  all: {
 		    files: ['cards/**/*.*', 'config/**/*.*', 'pages/**/*.*', 'static/**/*.*', 'partials/**/*.*'],
-		    tasks: ['webpack', 'jambobuild'],
+		    tasks: ['jambobuild', 'webpack',],
 		    options: {
 		      spawn: false,
 		    },
