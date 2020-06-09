a80f795bdc4d0edb54c4a02e29970a36016e14e6
Alexis Grow <agrow@yext.com>
2020-06-09 11:45:11 -0600

    fix all partial, add function comments

--- directanswercards/card_component.js
+++ directanswercards/card_component.js
@@ -10,8 +10,15 @@ BaseDirectAnswerCard['{{componentName}}'] = class extends ANSWERS.Component {
     this.answer = data.answer || {};
     this.relatedItem = data.relatedItem || {};
     this.type = data.type || '';
+    this.associatedEntityId = data.relatedItem && data.relatedItem.data && data.relatedItem.data.id;
   }
 
+  /**
+   * Transforms the state. The object returned from this method is the state for the component.
+   * The state is passed directly to the template renderer.
+   *
+   * @param {Object} data
+   */
   setState(data) {
     let cardData = this.dataForRender(this.type, this.answer, this.relatedItem);
     this.validateDataForRender(cardData);
@@ -28,8 +35,14 @@ BaseDirectAnswerCard['{{componentName}}'] = class extends ANSWERS.Component {
     }
   }
 
+
+  /**
+   * Returns an object with the default event options for the card, including
+   * any event options passed in
+   * @param {Object} eventOptions
+   */
   addDefaultEventOptions(eventOptions = {}) {
-    const entityId = this.relatedItem.data && this.relatedItem.data.id ? { entityId: this.relatedItem.data.id } : {};
+    let entityId = this.associatedEntityId ? { entityId: this.associatedEntityId } : {};
     return Object.assign({}, {
         searcher: "UNIVERSAL",
         ...entityId,
@@ -38,10 +51,17 @@ BaseDirectAnswerCard['{{componentName}}'] = class extends ANSWERS.Component {
     );
   }
 
+  /**
+   * Returns the type of component
+   */
   static get type() {
     return `{{componentName}}`;
   }
 
+  /**
+   * Returns a boolean indicating whether there can be multiple components of this
+   * type with the same name.
+   */
   static areDuplicateNamesAllowed() {
     return true;
   }
