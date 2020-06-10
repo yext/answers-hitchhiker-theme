3b6ec8fec91b58320ffe535736e33b9f01c8d5c0
Tom Meyer <tmeyer@yext.com>
2020-06-10 15:49:13 -0400

    Use TerserPlugin for minimization in the Webpack task. (#178)
    * Use TerserPlugin for minimization in the Webpack task.
    
    This PR updates the Webpack task to use the TerserPlugin when minimizing the
    JS bundle. The TerserPlugin is used by default in Webpack. Previously, we used
    the Closure compiler to optimize our JS. However, the compiler's plugin was
    slow to run. On average, the Webpack task took about 10 seconds when Closure
    was used. The task takes, on average, 6 seconds with the TerserPlugin. What's
    more, the TerserPlugin seems to provide a smaller sized bundle than Closure.
    
    J=SPR-2407
    TEST=compile
    
    Made sure the Webpack task produced the JS bundle successfully when using the
    TerserPlugin for minimization.

--- static/webpack-config.js
+++ static/webpack-config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const fs = require('file-system');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ClosurePlugin = require('closure-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = function () {
@@ -37,9 +36,6 @@ module.exports = function () {
         ['JAMBO_INJECTED_DATA']
       ),
     ],
-    optimization: {
-      minimizer: [new ClosurePlugin()]
-    },
     module: {
       rules: [
         {
