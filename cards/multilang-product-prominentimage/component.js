cff977559646cf430dedebcb79156979f81c8a66
Alexis Grow <agrow@yext.com>
2020-08-26 09:38:32 -0400

    Add multilanguage cards (#298)
    Add a multilanguage version of every existing card in the theme. These multilanguage cards have follow the naming scheme multilang-[the same name as their non-multilang counterparts]. All hardcoded content is wrapped in the Jambo translation helpers.
    
    J=SLAP-465

---
+++ cards/multilang-product-prominentimage/component.js
@@ -0,0 +1,68 @@
+{{> cards/card_component componentName='multilang-product-prominentimage' }}
+
+class multilang_product_prominentimageCardComponent extends BaseCard['multilang-product-prominentimage'] {
+  constructor(config = {}, systemConfig = {}) {
+    super(config, systemConfig);
+  }
+
+  /**
+   * This returns an object that will be called `card`
+   * in the template. Put all mapping logic here.
+   *
+   * @param profile profile of the entity in the card
+   */
+  dataForRender(profile) {
+    return {
+      title: profile.name, // The header text of the card
+      url: profile.landingPageUrl, // If the card title is a clickable link, set URL here
+      target: '_top', // If the title's URL should open in a new tab, etc.
+      titleEventOptions: this.addDefaultEventOptions(),
+      subtitle: profile.c_price ? `$${profile.c_price}` : '', // The sub-header text of the card
+      image: Formatter.image(profile.c_photo).url, // The URL of the image to display on the card
+      altText: Formatter.image(profile.c_photo).alternateText,  // The alternate text for the image
+      details: profile.description, // The text in the body of the card
+      // If the card's details are longer than a certain character count, you can truncate the
+      // text. A toggle will be supplied that can show or hide the truncated text.
+      showMoreDetails: {
+        showMoreLimit: 350, // Character count limit
+        showMoreText: {{ translateJS phrase='Show more' }}, // Label when toggle will show truncated text
+        showLessText: {{ translateJS phrase='Show less' }} // Label when toggle will hide truncated text
+      },
+      // The primary CTA of the card
+      CTA1: {
+        label: profile.c_primaryCTA ? profile.c_primaryCTA.label : null, // The CTA's label
+        iconName: 'chevron', // The icon to use for the CTA
+        url: Formatter.generateCTAFieldTypeLink(profile.c_primaryCTA), // The URL a user will be directed to when clicking
+        target: '_top', // Where the new URL will be opened
+        eventType: 'CTA_CLICK', // Type of Analytics event fired when clicking the CTA
+        eventOptions: this.addDefaultEventOptions(),
+        // ariaLabel: '', // Accessible text providing a descriptive label for the CTA
+      },
+      // The secondary CTA of the card
+      CTA2: {
+        label: profile.c_secondaryCTA ? profile.c_secondaryCTA.label : null,
+        iconName: 'chevron',
+        url: Formatter.generateCTAFieldTypeLink(profile.c_secondaryCTA),
+        target: '_top',
+        eventType: 'CTA_CLICK',
+        eventOptions: this.addDefaultEventOptions(),
+        // ariaLabel: '',
+      }
+    };
+  }
+
+  /**
+   * The template to render
+   * @returns {string}
+   * @override
+   */
+  static defaultTemplateName (config) {
+    return 'cards/multilang-product-prominentimage';
+  }
+}
+
+ANSWERS.registerTemplate(
+  'cards/multilang-product-prominentimage',
+  `{{{read 'cards/multilang-product-prominentimage/template' }}}`
+);
+ANSWERS.registerComponentType(multilang_product_prominentimageCardComponent);
