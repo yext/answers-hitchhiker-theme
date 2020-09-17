cd3889e0b9eab774f6565dec6c3cabfcbc6c0640
Oliver Shi <oshi@yext.com>
2020-09-17 12:18:22 -0400

    change script/* extensions from hbs to js

--- templates/vertical-map-filters/script/searchbar.js
+++ templates/vertical-map-filters/script/searchbar.js
@@ -1,6 +1,7 @@
-ANSWERS.addComponent("SearchBar", Object.assign({}, {
+ANSWERS.addComponent("SearchBar", {
   container: "#js-answersSearchBar",
   {{#if verticalKey}}
     verticalKey: "{{{verticalKey}}}",
   {{/if}}
-}, {{{ json componentSettings.SearchBar }}}));
+  ...{{{ json componentSettings.SearchBar }}}
+});
