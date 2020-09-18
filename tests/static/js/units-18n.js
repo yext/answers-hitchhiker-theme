3b3c0042dd1e72e682c21091f54bfc88c7f8ef3e
Connor Anderson <canderson@yext.com>
2020-09-17 20:01:23 -0400

    Improve tests

--- tests/static/js/units-18n.js
+++ tests/static/js/units-18n.js
@@ -1,18 +1,18 @@
 import { getDistanceUnit } from 'static/js/units-i18n.js';
 
 describe('getDistanceUnit', () => {
-  it('A known locale and know region work', () => {
+  it('A known locale works', () => {
     const unit = getDistanceUnit('en-GB');
     expect(unit).toEqual('km');
   });
 
-  it('A known locale and unknown region returns the default for the language', () => {
+  it('A known language and unknown region returns the default for that language', () => {
     const unit = getDistanceUnit('en-AA');
     expect(unit).toEqual('mi');
   });
 
   it('An unkown language defaults to imperial units', () => {
-    const unit = getDistanceUnit('en-AA');
+    const unit = getDistanceUnit('ye');
     expect(unit).toEqual('mi');
   });
 });
