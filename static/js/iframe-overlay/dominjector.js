fac16120ca2b4bf0b87a1c3cab81055bae635003
Alexis Grow <agrow@yext.com>
2020-10-09 15:44:12 -0400

    Set up communication between iFrame and parent frame (#361)
    Creates classes to listen to events on the parent frame (ParentFrameObserver), child iframe (IFrameObserver), and update the overlay if there are any relevant interactions from the observers (InteractionDirector). Add placeholder classes/methods for a overlay resizer class and a DOM injector class.
    
    TEST=manual,compile
    
    Test that jambo build && grunt webpack compiles with just this code. Test existing prototype with this code and verify expected behavior.

---
+++ static/js/iframe-overlay/dominjector.js
@@ -0,0 +1,12 @@
+/**
+ * DomInjector is responsible for injecting the elements for the Overlay into the
+ * document.
+ */
+export default class DomInjector {
+  /**
+   * Injects the Overlay elements into the DOM.
+   */
+  inject() {
+    // TODO (agrow) implement in a later PR
+  }
+}
---
+++ static/js/iframe-overlay/iframeobserver.js
@@ -0,0 +1,45 @@
+require('iframe-resizer');
+
+import InteractionDirector from './interactiondirector';
+import { InteractionTypes } from './constants';
+
+/**
+ * IFrameObserver is responsible for observing its iframe, notifying its mediator when
+ * the iframe changes state.
+ */
+export default class IFrameObserver {
+  constructor (mediator, iframeSelector) {
+    /**
+     * @type {InteractionDirector}
+     */
+    this.mediator = mediator;
+
+    /**
+     * @type {String}
+     */
+    this.iframeSelector = iframeSelector;
+  }
+
+  /**
+   * Observes the iframe and notifies the mediator when the iframe sends messages or
+   * is initialized.
+   */
+  attach() {
+    iFrameResize({
+      checkOrigin: false,
+      sizeHeight: false,
+      autoResize: false,
+      scrolling: true,
+      onInit: () => {
+        this.mediator.onIFrameInteraction(InteractionTypes.INIT);
+      },
+      onMessage: (data) => {
+        if (!data.message) {
+          return;
+        }
+
+        this.mediator.onIFrameInteraction(data.message.type, data.message.detail);
+      }
+    }, this.iframeSelector);
+  }
+}
