69c322940bf76c5c1e3a2097d2c6abba01aecd80
Alexis Grow <agrow@yext.com>
2020-10-09 15:44:12 -0400

    Set up communication between iFrame and parent frame (#361)
    Creates classes to listen to events on the parent frame (ParentFrameObserver), child iframe (IFrameObserver), and update the overlay if there are any relevant interactions from the observers (InteractionDirector). Add placeholder classes/methods for a overlay resizer class and a DOM injector class.
    
    TEST=manual,compile
    
    Test that jambo build && grunt webpack compiles with just this code. Test existing prototype with this code and verify expected behavior.

---
+++ static/js/iframe-overlay/parentframeobserver.js
@@ -0,0 +1,56 @@
+import { InteractionTypes } from './constants';
+import InteractionDirector from './interactiondirector';
+
+/**
+ * ParentFrameObserver observes the parent frame and notifies its mediator whenever
+ * an interaction occurs that affects the Overlay.
+ */
+export default class ParentFrameObserver {
+  constructor(mediator, customButtonSelector) {
+    /**
+     * @type {InteractionDirector}
+     */
+    this.mediator = mediator;
+
+    /**
+     * @type {Element}
+     */
+    this.customButtonEl = customButtonSelector &&
+      document.querySelector(customButtonSelector);
+  }
+
+  /**
+   * Attaches event listeners to elements in the parent frame that affect the Overlay.
+   */
+  attach() {
+    this.customButtonEl && this._initCustomButton();
+    this._initGenericClickListener();
+  }
+
+  /**
+   * Adds event listener to collapse the overlay on parent frame clicks that are outside
+   * the custom button element, if provided. (By definition, these click events will not
+   * include clicks within any child iframes.)
+   *
+   * @param {Element} customButtonEl
+   */
+  _initGenericClickListener() {
+    window.addEventListener('click', (e) => {
+      const isButtonClick = this.customButtonEl && this.customButtonEl.contains(e.target);
+
+      if (!isButtonClick) {
+        this.mediator.onParentFrameInteraction(InteractionTypes.COLLAPSE);
+      }
+    });
+  }
+
+  /**
+   * Adds event listener to the custom button element, if provided, so the custom
+   * button can trigger overlay collapsing and expanding.
+   */
+  _initCustomButton() {
+    this.customButtonEl.addEventListener('click', () => {
+      this.mediator.onParentFrameInteraction(InteractionTypes.TOGGLE_OVERLAY);
+    });
+  }
+}
