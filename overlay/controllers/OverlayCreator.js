5105bb7b505175b1794323c876cdf5e7f826d06c
Alexis Grow <agrow@yext.com>
2020-10-14 14:16:12 -0400

    Add Overlay initialization to theme (#373)
    Adds class to inject text, colors, and other styles from the Overlay config. Adds placeholder classes to inject content into each section of the overlay: header panel, buttons, and prompts.
    
    TEST=manual,compile
    
    Test with existing code in Overlay to confirm correct behavior. Run `jambo build` with this code alone to ensure it compiles separately.

---
+++ overlay/controllers/OverlayCreator.js
@@ -0,0 +1,70 @@
+/**
+ * This class is responsible for using the overlay config to create and/or set up
+ * the portions of the page necessary for the Overlay
+ */
+class OverlayCreator {
+  constructor(config = {}) {
+    /**
+     * @type {Object}
+     */
+    this._buttonConfig = {
+      labelText: config.button.text,
+      alignment: config.button.alignment,
+      backgroundColor: config.button.backgroundColor,
+      foregroundColor: config.button.foregroundColor,
+      borderRadius: config.button.text ? '200px' : '50%'
+    };
+
+    /**
+     * @type {Object}
+     */
+    this._panelConfig = {
+      heading: config.panel.header,
+      subtitle: config.panel.subtitle,
+      icon: config.panel.icon,
+      backgroundColor: config.panel.backgroundColor,
+      foregroundColor: config.panel.foregroundColor
+    };
+
+    /**
+     * @type {Array<Object>}
+     */
+    this._prompts = config.prompts;
+
+    /**
+     * @type {boolean}
+     */
+    this._isCollapsed = true; // TODO (agrow) get this value from config
+  }
+
+  /**
+   * Creates and sets up the portions of the page specific to the overlay: header panel,
+   * prompts, and button
+   */
+  create() {
+    const bodyEl = document.querySelector('body');
+    bodyEl.classList.add('Overlay');
+
+    window.collapseOverlay();
+
+    new HeaderPanelInjector(this._panelConfig).inject();
+    new PromptInjector(this._prompts).inject();
+    new ButtonInjector(this._buttonConfig).inject();
+
+    this._updateOverlayShape();
+  }
+
+  /**
+   * Updates the initial state of the page based on whether the Overlay is expanded
+   * or collapsed.
+   */
+  _updateOverlayShape() {
+    if (this._isCollapsed) {
+      window.shrinkOverlay();
+      window.collapseOverlay();
+    } else {
+      window.growOverlay();
+      window.expandOverlay();
+    }
+  }
+}
