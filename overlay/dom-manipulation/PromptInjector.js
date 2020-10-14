3d1878555f27d3b2904a37924b804a1f1f5ff144
Alexis Grow <agrow@yext.com>
2020-10-14 14:16:12 -0400

    Add Overlay initialization to theme (#373)
    Adds class to inject text, colors, and other styles from the Overlay config. Adds placeholder classes to inject content into each section of the overlay: header panel, buttons, and prompts.
    
    TEST=manual,compile
    
    Test with existing code in Overlay to confirm correct behavior. Run `jambo build` with this code alone to ensure it compiles separately.

---
+++ overlay/dom-manipulation/PromptInjector.js
@@ -0,0 +1,28 @@
+/**
+ * This class is responsible for injecting any the prompts from the Overlay config into
+ * the DOM.
+ */
+class PromptInjector {
+  /**
+   * Injects the prompts into the DOM.
+   */
+  inject() {
+    // TODO (agrow) implement in a later PR
+    this._injectPrompts();
+    this._attachEventListeners();
+  }
+
+  /**
+   * Injects prompts as buttons or links into the DOM, based on their configuration.
+   */
+  _injectPrompts() {
+    // TODO (agrow) implement in a later PR
+  }
+
+  /**
+   * Attaches the Overlay event listeners to the button
+   */
+  _attachEventListeners() {
+    // TODO (agrow) implement in a later PR
+  }
+}
