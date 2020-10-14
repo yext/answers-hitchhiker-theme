a84484c7bcd49500d40ca1717cfdf23cfb913bb5
Alexis Grow <agrow@yext.com>
2020-10-14 11:58:55 -0400

    Add iframe message handler to layout (#366)
    TEST=manual
    
    Test using the other overlay prototype code. Serve webpage, test click events that should expand, collapse, grow and shrink the overlay.

---
+++ overlay/controllers/shape.js
@@ -0,0 +1,38 @@
+window.shrinkOverlay = function () {
+  const bodyEl = document.querySelector('body');
+
+  if (bodyEl) {
+    bodyEl.classList.add('shorter');
+    bodyEl.classList.remove('taller');
+  }
+}
+
+window.growOverlay = function () {
+  const bodyEl = document.querySelector('body');
+
+  if (bodyEl) {
+    bodyEl.classList.add('taller');
+    bodyEl.classList.remove('shorter');
+  }
+}
+
+window.collapseOverlay = function () {
+  const bodyEl = document.querySelector('body');
+
+  if (bodyEl) {
+    bodyEl.classList.add('collapsed');
+    bodyEl.classList.remove('expanded');
+  }
+}
+
+window.expandOverlay = function () {
+  const bodyEl = document.querySelector('body');
+
+  if (bodyEl) {
+    bodyEl.classList.remove('collapsed');
+    bodyEl.classList.add('expanded');
+
+    const inputEl = document.querySelector('.js-yext-query');
+    inputEl && inputEl.focus();
+  }
+}
