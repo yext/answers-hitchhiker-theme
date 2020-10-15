9533c8617d62213730adb86bd3c1d70bd799b765
Alexis Grow <agrow@yext.com>
2020-10-15 10:34:04 -0400

    Implement class for controlling sizing of overlay (#371)
    TEST=manual
    
    Test with existing Overlay prototype code and code from pending PRs, test flow of collapsing, expanding, searching, and clearing search to confirm expected behavior.

--- static/js/iframe-overlay/dom/expando.js
+++ static/js/iframe-overlay/dom/expando.js
@@ -1,10 +1,30 @@
-import { ActionTypes } from "../constants";
+import { ActionTypes, AnimationStyling, Selectors } from '../constants';
 
 /**
  * Expando is responsible for handling the resizing of the Overlay.
  */
 export default class Expando {
   constructor() {
+    /**
+     * @type {Element}
+     */
+    this._iframeEl = document.querySelector(`#${Selectors.IFRAME_ID}`);
+
+    /**
+     * @type {Element}
+     */
+    this._iframeWrapperEl = document.querySelector(`#${Selectors.IFRAME_CONTAINER_ID}`);
+
+    /**
+     * @type {boolean}
+     */
+    this._isExpanded = false;
+
+    /**
+     * @type {boolean}
+     */
+    this._isTaller = true;
+
     /**
      * @type {function}
      */
@@ -22,14 +42,42 @@ export default class Expando {
    * @param {Object} size
    */
   showButton(size) {
-    // TODO (agrow) implement in a later PR
+    this._isExpanded = true;
+    this._buttonWidth = size.width;
+    this._buttonHeight = size.height;
+    this._shorterHeight = `${size.totalHeight}px`;
+
+    this._addMediaQueryListener();
+    this.shrink();
+    this.collapse();
+
+    setTimeout(() => {
+      this._iframeWrapperEl.style['z-index'] = '2147483639';
+      this._iframeWrapperEl.style['opacity'] = '1';
+    }, 250);
   }
 
   /**
    * Collapses the Overlay
    */
   collapse() {
-    // TODO (agrow) implement in a later PR
+    if (!this._isExpanded) {
+      return;
+    }
+    this._isExpanded = false;
+
+    this._iframeEl.scrolling = 'no';
+    this._iframeEl.style['background-color'] = 'transparent';
+    this._iframeEl.style['transition'] = `background-color ${AnimationStyling.FADE_TIMING}`;
+    this._iframeEl.style['transition-delay'] = '0s';
+
+    this._iframeWrapperEl.style['transition'] =
+      `height ${AnimationStyling.SIZE_TIMING}, width ${AnimationStyling.SIZE_TIMING}`;
+    this._iframeWrapperEl.style['transition-delay'] = AnimationStyling.FADE_TIMING;
+    this._iframeWrapperEl.style['width'] = this._buttonWidth;
+    this._iframeWrapperEl.style['height'] = this._buttonHeight;
+    this._iframeWrapperEl.style['box-shadow'] = AnimationStyling.BOX_SHADOW_NONE;
+
     this._collapseCallback();
   }
 
@@ -37,7 +85,24 @@ export default class Expando {
    * Expands the overlay
    */
   expand() {
-    // TODO (agrow) implement in a later PR
+    if (this._isExpanded) {
+      return;
+    }
+    this._isExpanded = true;
+
+    this._iframeEl.scrolling = 'yes';
+    this._iframeEl.style['transition'] = `background-color ${AnimationStyling.FADE_TIMING}`;
+    this._iframeEl.style['transition-delay'] = AnimationStyling.SIZE_TIMING;
+    this._iframeEl.style['background-color'] = AnimationStyling.BACKGROUND_COLOR_NORMAL;
+
+    this._iframeWrapperEl.style['transition'] = `box-shadow ${AnimationStyling.SIZE_TIMING}`;
+    this._iframeWrapperEl.style['transition-delay'] = AnimationStyling.SIZE_TIMING;
+    this._iframeWrapperEl.style['box-shadow'] = AnimationStyling.BOX_SHADOW_NORMAL;
+    this._iframeWrapperEl.style['width'] = this._overlayWidth;
+    this._iframeWrapperEl.style['height'] = this._isTaller
+      ? AnimationStyling.HEIGHT_TALLER
+      : this._shorterHeight;
+
     this._expandCallback();
   }
 
@@ -45,14 +110,30 @@ export default class Expando {
    * Makes the overlay grow to its larger size
    */
   grow() {
-    // TODO (agrow) implement in a later PR
+    if (this._isTaller) {
+      return;
+    }
+    this._isTaller = true;
+
+    this._iframeWrapperEl.style['transition'] = `${AnimationStyling.SIZE_TIMING} ease all`;
+    this._iframeWrapperEl.style['width'] = this._overlayWidth;
+    this._iframeWrapperEl.style['height'] = AnimationStyling.HEIGHT_TALLER;
+    this._iframeWrapperEl.style['box-shadow'] = AnimationStyling.BOX_SHADOW_NORMAL;
   }
 
   /**
    * Shrinks the overlay to its shorter size
    */
   shrink() {
-    // TODO (agrow) implement in a later PR
+    if (!this._isTaller) {
+      return;
+    }
+    this._isTaller = false;
+
+    this._iframeWrapperEl.style['transition'] = `${AnimationStyling.SIZE_TIMING} ease all`;
+    this._iframeWrapperEl.style['width'] = this._overlayWidth;
+    this._iframeWrapperEl.style['height'] = this._shorterHeight;
+    this._iframeWrapperEl.style['box-shadow'] = AnimationStyling.BOX_SHADOW_NORMAL;
   }
 
   /**
@@ -73,5 +154,29 @@ export default class Expando {
         console.warn(`Callback type '${type}' not supported.`);
         break;
     }
+  }
+
+  /**
+   * Updates the Overlay sizing values (if necessary) when the viewport size changes.
+   */
+  _addMediaQueryListener() {
+    const mediaQuery = window.matchMedia("(max-width: 767px)");
+    this._updateOverlayWidth(mediaQuery.matches);
+
+    mediaQuery.addListener((e) => {
+      this._updateOverlayWidth(e.matches);
+    });
+  }
+
+  /**
+   * Updates the Overlay width depending on whether the window size is mobile-sized or
+   * not
+   *
+   * @param {boolean} isMobile
+   */
+  _updateOverlayWidth(isMobile) {
+    this._overlayWidth = isMobile
+      ? AnimationStyling.WIDTH_MOBILE
+      : AnimationStyling.WIDTH_DESKTOP;
   }
 }
