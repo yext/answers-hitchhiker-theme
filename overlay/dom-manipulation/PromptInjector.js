a83fd6a390cca35c21c90793b0151c8bbd5e1d4b
Alexis Grow <agrow@yext.com>
2020-10-16 12:23:38 -0400

    Inject prompts from Overlay config into the DOM (#383)
    Implement the PromptInjector class which injects prompt elements within in the iframe. The prompt element info (text, url (optional), target (optional)) comes from a config object passed from the parent frame. We don't know how many prompts there will be, so they've got to be dynamically added when the information arrives from the parent frame.
    
    TEST=manual
    
    Test with other code on feature/overlay branch and pending PRs, confirm prompts are injected and look visually correct.

--- overlay/dom-manipulation/PromptInjector.js
+++ overlay/dom-manipulation/PromptInjector.js
@@ -3,11 +3,26 @@
  * the DOM.
  */
 class PromptInjector {
+  constructor(prompts = []) {
+    /**
+     * @type {Array<Object>}
+     */
+    this.prompts = prompts;
+
+    /**
+     * @type {Element}
+     */
+    this.promptsWrapperEl = document.querySelector('.js-OverlayPromptButtons');
+  }
+
   /**
-   * Injects the prompts into the DOM.
+   * Inserts prompts and adds event listeners to handle prompt button interactions
    */
   inject() {
-    // TODO (agrow) implement in a later PR
+    if (!this.promptsWrapperEl || this.prompts.length <= 0) {
+      return;
+    }
+
     this._injectPrompts();
     this._attachEventListeners();
   }
@@ -16,13 +31,101 @@ class PromptInjector {
    * Injects prompts as buttons or links into the DOM, based on their configuration.
    */
   _injectPrompts() {
-    // TODO (agrow) implement in a later PR
+    for (const prompt of this.prompts) {
+      if (!prompt.text) {
+        continue;
+      }
+
+      const promptEl = prompt.url
+        ? this._createLinkEl(prompt.url, prompt.target)
+        : this._createButtonEl();
+
+      this.promptsWrapperEl.appendChild(promptEl);
+      this._appendPromptText(promptEl, prompt.text);
+      this._appendChevronRight(promptEl);
+    }
   }
 
   /**
-   * Attaches the Overlay event listeners to the button
+   * Attaches the Overlay event listeners to the prompt elements
    */
   _attachEventListeners() {
-    // TODO (agrow) implement in a later PR
+    const promptLinkEls = document.querySelectorAll('.js-OverlayPrompt-link');
+    for (const promptLinkEl of promptLinkEls) {
+      promptLinkEl.addEventListener('click', () => {
+        window.parentIFrame.sendMessage({
+          type: 'querySubmitted'
+        });
+      })
+    }
+
+    const promptButtonEls = document.querySelectorAll('.js-OverlayPrompt-button');
+    for (const promptButtonEl of promptButtonEls) {
+      promptButtonEl.addEventListener('click', function() {
+        ANSWERS.search(promptButtonEl.innerText);
+
+        window.parentIFrame.sendMessage({
+          type: 'querySubmitted'
+        });
+
+        window.growOverlay();
+      });
+    }
+  }
+
+  /**
+   * Creates a prompt button element
+   *
+   * @returns {Element}
+   */
+  _createButtonEl() {
+    const promptEl = document.createElement('button');
+    promptEl.classList.add('OverlayPrompt-button');
+    promptEl.classList.add('js-OverlayPrompt-button');
+    return promptEl;
+  }
+
+  /**
+   * Creates a prompt link element
+   *
+   * @param {string} url The value for the href of the link
+   * @param {string} target The link target
+   * @returns {Element}
+   */
+  _createLinkEl(url, target) {
+    const promptEl = document.createElement('a');
+    promptEl.classList.add('OverlayPrompt-button');
+    promptEl.classList.add('js-OverlayPrompt-link');
+    promptEl.href = url;
+    if (target) {
+      promptEl.target = target;
+    }
+    return promptEl;
+  }
+
+  /**
+   * Appends an element with prompt text into the given element
+   *
+   * @param {Element} el
+   * @param {string} promptText
+   */
+  _appendPromptText(el, promptText) {
+    const promptEl = document.createElement('div');
+    promptEl.classList.add('OverlayPrompt-label');
+    promptEl.innerText = promptText;
+    el.appendChild(promptEl);
+  }
+
+  /**
+   * Appends an element with a chevron-right icon to the given element
+   *
+   * @param {Element} el
+   */
+  _appendChevronRight(el) {
+    const iconEl = document.createElement('div');
+    iconEl.classList.add('OverlayPrompt-buttonIcon');
+    iconEl.innerHTML = `{{> overlay/icons/chevron-right }}`;
+
+    el.appendChild(iconEl);
   }
 }
