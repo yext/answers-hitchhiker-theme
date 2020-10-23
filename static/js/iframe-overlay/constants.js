820b167d99b4e70833dcce69ea433fd43d4184cd
Alexis Grow <agrow@yext.com>
2020-10-23 15:18:16 -0400

    Update styling so mobile is full bleed

--- static/js/iframe-overlay/constants.js
+++ static/js/iframe-overlay/constants.js
@@ -24,7 +24,12 @@ exports.AnimationStyling = {
   BOX_SHADOW_NORMAL: '0 3px 10px 0 rgba(0,0,0,0.4)',
   BOX_SHADOW_NONE: '0 3px 10px 0 rgba(0,0,0,0)',
   WIDTH_DESKTOP: '445px',
-  WIDTH_MOBILE: 'calc(100% - 32px)',
+  WIDTH_MOBILE: '100vw',
   MIN_HEIGHT: 525,
+  MAX_HEIGHT_DESKTOP: 'calc(100% - 32px)',
+  MAX_WIDTH_DESKTOP: 'calc(100% - 32px)',
+  MAX_HEIGHT_MOBILE: '100%',
+  MAX_WIDTH_MOBILE: '100%',
+  BASE_SPACING: '16px',
   HEIGHT_TALLER: 'calc(100% - 32px)'
 };
--- static/js/iframe-overlay/dom/expando.js
+++ static/js/iframe-overlay/dom/expando.js
@@ -177,10 +177,12 @@ export default class Expando {
    */
   _addMediaQueryListener() {
     const mediaQuery = window.matchMedia("(max-width: 767px)");
-    this._updateOverlayWidth(mediaQuery.matches);
+    this._updateCalculatedWidth(mediaQuery.matches);
+    this._updateOverlayContainerSpacing(mediaQuery.matches);
 
     mediaQuery.addListener((e) => {
-      this._updateOverlayWidth(e.matches);
+      this._updateCalculatedWidth(e.matches);
+      this._updateOverlayContainerSpacing(e.matches);
     });
   }
 
@@ -190,9 +192,29 @@ export default class Expando {
    *
    * @param {boolean} isMobile
    */
-  _updateOverlayWidth(isMobile) {
+  _updateCalculatedWidth(isMobile) {
     this._overlayWidth = isMobile
       ? AnimationStyling.WIDTH_MOBILE
       : AnimationStyling.WIDTH_DESKTOP;
+  }
+
+  /**
+   * Updates the Overlay container's width depending on whether the window size is mobile-sized
+   * or not
+   *
+   * @param {boolean} isMobile
+   */
+  _updateOverlayContainerSpacing(isMobile) {
+    if (isMobile) {
+      this._overlayContainerEl.style[this._alignment] = 0;
+      this._overlayContainerEl.style['bottom'] = 0;
+      this._overlayContainerEl.style['max-width'] = AnimationStyling.MAX_WIDTH_MOBILE;
+      this._overlayContainerEl.style['max-height'] = AnimationStyling.MAX_HEIGHT_MOBILE;
+    } else {
+      this._overlayContainerEl.style[this._alignment] = AnimationStyling.BASE_SPACING;
+      this._overlayContainerEl.style['bottom'] = AnimationStyling.BASE_SPACING;
+      this._overlayContainerEl.style['max-width'] = AnimationStyling.MAX_WIDTH_DESKTOP;
+      this._overlayContainerEl.style['max-height'] = AnimationStyling.MAX_HEIGHT_DESKTOP;
+    }
   }
 }
