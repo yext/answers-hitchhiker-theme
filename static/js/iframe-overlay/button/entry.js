0da0ecda7f3816cb7101235666efa6a18e16fe05
Alexis Grow <agrow@yext.com>
2020-10-23 10:27:02 -0400

    Overlay: move button to separate iframe (#407)
    Create a separate HTML asset for the button iframe. Add JS and CSS output files for the new frame. There will be another PR after this one that replaces the usages of the old button with this new one.
    
    TEST=manual
    
    Run npx jambo build && grunt webpack to build site. Inspect output files and see three new ones: overlay-button.html, overlay-button.css and overlay-button.js. Reference the new overlay-button.html file in my test site and see it appear styled correctly and that click events fire as they should.

---
+++ static/js/iframe-overlay/button/entry.js
@@ -0,0 +1,10 @@
+// Import global polyfills
+import 'core-js/stable';
+
+// Import all SCSS
+import Scss from '../../../scss/answers/overlay/button.scss';
+
+// Import all JS assets
+import OverlayButtonJS from './button.js';
+export { OverlayButtonJS };
+window.OverlayButtonJS = OverlayButtonJS;
