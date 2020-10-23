60c6b42d93995970823a38ce54cde2727a4cd3b0
Alexis Grow <agrow@yext.com>
2020-10-23 10:27:02 -0400

    Overlay: move button to separate iframe (#407)
    Create a separate HTML asset for the button iframe. Add JS and CSS output files for the new frame. There will be another PR after this one that replaces the usages of the old button with this new one.
    
    TEST=manual
    
    Run npx jambo build && grunt webpack to build site. Inspect output files and see three new ones: overlay-button.html, overlay-button.css and overlay-button.js. Reference the new overlay-button.html file in my test site and see it appear styled correctly and that click events fire as they should.

---
+++ static/js/iframe-overlay/button/button.js
@@ -0,0 +1,61 @@
+export default class OverlayButtonJS {
+  /**
+   * Updates the button to its collapsed state
+   *
+   * @param {Element} buttonEl
+   */
+  static collapseButton(buttonEl) {
+    buttonEl.classList.add('collapsed');
+  }
+
+  /**
+   * Updates the button to its expanded state
+   *
+   * @param {Element} buttonEl
+   */
+  static expandButton(buttonEl) {
+    buttonEl.classList.remove('collapsed');
+  }
+
+  /**
+   * Injects styling for the background and foreground colors
+   *
+   * @param {Element} buttonEl
+   * @param {string} backgroundColor
+   * @param {string} foregroundColor
+   */
+  static applyStyling(buttonEl, backgroundColor, foregroundColor) {
+    // Add background color
+    const bodyEl = document.querySelector('body');
+    bodyEl && (bodyEl.style['background-color'] = backgroundColor);
+    buttonEl && (buttonEl.style['background-color'] = backgroundColor);
+
+    // Add foreground color
+    buttonEl && (buttonEl.style['color'] = foregroundColor);
+    const buttonIconEls = buttonEl.querySelectorAll('.js-OverlayButton-icon svg');
+    for (const buttonIconEl of buttonIconEls) {
+      buttonIconEl.style.fill = foregroundColor;
+    }
+  };
+
+  /**
+   * Attaches the Overlay event listeners to the button
+   *
+   * @param {Element} buttonEl
+   */
+  static attachEventListeners(buttonEl) {
+    buttonEl.addEventListener('click', function () {
+      const isCollapsed = buttonEl.classList.contains('collapsed');
+      if (isCollapsed) {
+        OverlayButtonJS.expandButton(buttonEl);
+      } else {
+        OverlayButtonJS.collapseButton(buttonEl);
+      }
+
+      const messageType = isCollapsed ? 'expand' : 'collapse';
+      window.parentIFrame.sendMessage({
+        type: messageType
+      });
+    });
+  }
+}
