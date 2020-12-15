ecb712062cffaea97080846f7403088cd6ae28b4
Oliver Shi <oshi@yext.com>
2020-12-15 14:39:27 -0500

    add templatedata hook
    This commit adds the templatedata hook. It currently
    mirrors what jambo will do by default. This commit
    has no functional differences with regards to the
    data passed to templates.
    
    J=SLAP-857
    TEST=manual
    
    added a custom helper in a test site that would
    fs.writeFile the current template data into a json file
    jambo build-ed a site with and without the templatedata hook,
    checked that the new and old json files were identical (eye test and
    also with jest)
    
    also did the above for yanswers index.html and index.ja.json

---
+++ hooks/templatedata.js
@@ -0,0 +1,30 @@
+/**
+ * Returns the data after performing the given template data hook 
+ * transformation on it.
+ *
+ * @param {Object} pageConfig the configuration for the current page
+ * @param {string} relativePath the relativePath for the current page
+ * @param {Object} params the params Object for the current locale, e.g. localeConfig.en.params
+ * @param {Object} globalConfig global_config.json
+ * @param {Object} pageNameToConfig object of page names to config objects
+ * @param {Object} env contains environment variables e.g. env.JAMBO_INJECTED_DATA
+ * @returns {Object} the additional params to add to the template data
+ */
+module.exports = function({
+  pageConfig,
+  relativePath,
+  params,
+  globalConfig,
+  pageNameToConfig,
+  env
+}) {
+  const data = {
+    ...pageConfig,
+    verticalConfigs: pageNameToConfig,
+    global_config: globalConfig,
+    relativePath,
+    params,
+    env
+  };
+  return data;
+}
