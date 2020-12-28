898a0f3afaf3a3939eebcc1c8d33e4a855248654
Oliver Shi <oshi@yext.com>
2020-12-28 13:41:55 -0500

    add templatedataformatter hook (#543)
    This commit adds the templatedata hook. It currently
    mirrors what jambo will do by default. This commit
    has no functional differences with regards to the
    data passed to templates.
    
    The jambo version will need to be bumped once
    the newest jambo (containing support for this hook)
    is released, for this to do anything.
    
    J=SLAP-857
    TEST=manual
    
    added a custom helper that would fs.writeFile the current template data
    into a json file
    jambo build-ed a site with and without the templatedata hook,
    checked that the 2 json files were identical (eye test + jest)
    then checkout current develop version of jambo and test that
    the template data without any of these changes was the same as the
    other 2
    
    also did the above for yanswers index, index.ja, index.en_GB
    also did the above for weight watchers repo when it was still single lang
    (no locale_config.json)
    
    test building a site with a local JAMBO_INJECTED_DATA and also with it unset

---
+++ hooks/templatedataformatter.js
@@ -0,0 +1,53 @@
+/**
+ * Formats the data sent to the handlebars templates during Jambo builds.
+ *
+ * @param {string} pageMetadata.relativePath relativePath from the page to the output dir
+ * @param {string} pageMetadata.pageName name of the page being build
+ 
+ * @param {Object} siteLevelAttributes.globalConfig global_config.json
+ * @param {Object} siteLevelAttributes.currentLocaleConfig the chunk of locale config for the current locale
+ * @param {string} siteLevelAttributes.locale the current locale
+ * @param {Object} siteLevelAttributes.env all environment variables, like JAMBO_INJECTED_DATA
+ * 
+ * @param {Object} pageNameToConfig object of pageName to pageConfig
+ * @returns {Object}
+ */
+module.exports = function (pageMetadata, siteLevelAttributes, pageNameToConfig) {
+  const { relativePath, pageName } = pageMetadata;
+  const { globalConfig, currentLocaleConfig, locale, env } = siteLevelAttributes;
+  const currentPageConfig = pageNameToConfig[pageName];
+  const templateData = {
+    ...currentPageConfig,
+    verticalConfigs: pageNameToConfig,
+    global_config: getLocalizedGlobalConfig(globalConfig, currentLocaleConfig, locale),
+    params: currentLocaleConfig.params || {},
+    relativePath,
+    env: {
+      JAMBO_INJECTED_DATA: env.JAMBO_INJECTED_DATA
+    }
+  };
+  return templateData;
+}
+
+/**
+ * Gets the global config, with experienceKey and locale added
+ * to it.
+ * 
+ * @param {Object} globalConfig 
+ * @param {Object} currentLocaleConfig chunk of locale config for the current locale
+ * @param {string} locale the current locale
+ * @returns {Object}
+ */
+function getLocalizedGlobalConfig(globalConfig, currentLocaleConfig, locale) {
+  const localizedGlobalConfig = {
+    ...globalConfig
+  };
+  const { experienceKey } = currentLocaleConfig;
+  if (experienceKey) {
+    localizedGlobalConfig.experienceKey = experienceKey;
+  }
+  if (locale) {
+    localizedGlobalConfig.locale = locale;
+  }
+  return localizedGlobalConfig;
+}
