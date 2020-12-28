500e49974186b69bd75a10981024a4ec156ae3dc
Connor Anderson <canderson@yext.com>
2020-12-28 16:39:51 -0500

    simplify scripts

--- testing/site/scripts/build.sh
+++ testing/site/scripts/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-set -e
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+set_working_dir_to_testing_site_dir () {
+  cd "$( dirname "${BASH_SOURCE[0]}" )"
+  cd ..
+}
 
-./setup.sh
+copy_static_files_into_working_dir () {
+  cp ../../static/Gruntfile.js Gruntfile.js
+  cp ../../static/package-lock.json package-lock.json
+  cp ../../static/package.json package.json
+  cp ../../static/webpack-config.js webpack-config.js
+}
 
-cd .. 
+set_working_dir_to_testing_site_dir
+copy_static_files_into_working_dir
 jambo build && grunt webpack
--- testing/site/scripts/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-./setup.sh
-
-cd ..
-npx serve -p 8000 public/ & grunt watch
