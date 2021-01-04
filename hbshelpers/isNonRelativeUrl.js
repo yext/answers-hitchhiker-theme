0e0cf43d84ea4c6fdba2bf46c924ce41bf774750
Oliver Shi <oshi@yext.com>
2021-01-04 17:51:57 -0500

    add isNonRelativeUrl

---
+++ hbshelpers/isNonRelativeUrl.js
@@ -0,0 +1,11 @@
+/**
+ * Determine whether a URL is absolute or not.
+ * Common examples: "mailto:slapshot@gmail.com", "//yext.com", "https://yext.com",
+ * "/my-img.svg"
+ * 
+ * @param {string} str the url to test
+ */
+module.exports = function isNonRelativeUrl(str) {
+  const absoluteURLRegex = /^(\/|[a-zA-Z]+:)/;
+  return str && str.match(absoluteURLRegex);
+}
