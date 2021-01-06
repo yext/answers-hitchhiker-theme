18d40ff3d3ba600953c6195ccfb04703a7b50442
Oliver Shi <oshi@yext.com>
2021-01-06 11:53:15 -0500

    update helper to match other pr

--- hbshelpers/isNonRelativeUrl.js
+++ hbshelpers/isNonRelativeUrl.js
@@ -1,6 +1,6 @@
 /**
- * Determine whether a URL is absolute or not.
- * Common examples: "mailto:slapshot@gmail.com", "//yext.com", "https://yext.com",
+ * Determines whether a URL is a non-relative url.
+ * Common non-relative examples: "mailto:slapshot@gmail.com", "//yext.com", "https://yext.com",
  * "/my-img.svg"
  * 
  * @param {string} str the url to test
