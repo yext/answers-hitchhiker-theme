26eb1c935d99ff7ba3a44cb18d21103a431cd7a3
Tom Meyer <tmeyer@yext.com>
2021-02-01 12:26:20 -0500

    Add the new `vertical` custom command to the Theme.
    This PR adds the new `vertical` custom commad. This command creates a page for
    the given vertical and sets the verticalKey and cardType placeholders in the
    JSON configuration. The command takes in the following arguments (all
    required): `name`, `template`, `cardName`, and `verticalKey`. A `locales`
    option was not included. This is because localized sites usually use different
    vertical keys and cards than their default locale counterparts.
    
    In the `describe` method, we provide the valid options for templates. The
    `page` command's logic for fetching the valid template types was reused.
    
    J=SLAP-1041
    TEST=manual
    
    Tested the following:
    - The output of `npx jambo vertical --help` was correct.
    - The output of `npx jambo describe` was correct for all commands, including
      `vertical`.
    - Running `npx jambo vertical` with the various flags set created a correctly
      configured vertical page.

---
+++ commands/addvertical.js
@@ -0,0 +1,137 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { spawnSync } = require('child_process');
+
+const { ArgumentMetadata, ArgumentType } = require('./helpers/utils/argumentmetadata');
+
+/**
+ * VerticalAdder represents the `vertical` custom jambo command. The command adds
+ * a new page for the given Vertical and associates a card type with it.
+ */
+class VerticalAdder {
+  constructor(jamboConfig) {
+    this.config = jamboConfig;
+  }
+
+  /**
+   * @returns {string} the alias for the add vertical command.
+   */
+  static getAlias() {
+    return 'vertical';
+  }
+
+  /**
+   * @returns {string} a short description of the add vertical command.
+   */
+  static getShortDescription() {
+    return 'create the page for a vertical';
+  }
+
+  /**
+   * @returns {Object<string, ArgumentMetadata>} description of each argument for 
+   *                                             the add vertical command, keyed by name
+   */
+  static args() {
+    return {
+      name: new ArgumentMetadata(ArgumentType.STRING, 'name of the vertical\'s page', true),
+      verticalKey: new ArgumentMetadata(ArgumentType.STRING, 'the vertical\'s key', true),
+      cardName: new ArgumentMetadata(
+        ArgumentType.STRING, 'card to use with vertical', true),
+      template: new ArgumentMetadata(
+        ArgumentType.STRING, 'page template to use within theme', true)
+    };
+  }
+
+  /**
+   * @returns {Object} description of the vertical command and its parameters.
+   */
+  static describe(jamboConfig) {
+    return {
+      displayName: 'Add Vertical',
+      params: {
+        name: {
+          displayName: 'Page Name',
+          required: true,
+          type: 'string'
+        },
+        verticalKey: {
+          displayName: 'Vertical Key',
+          required: true,
+          type: 'string',
+        },
+        cardName: {
+          displayName: 'Card Name',
+          required: true,
+          type: 'string'
+        },
+        template: {
+          displayName: 'Page Template',
+          required: true,
+          type: 'singleoption',
+          options: this._getPageTemplates(jamboConfig)
+        }
+      }
+    };
+  }
+
+  /**
+   * @returns {Array<string>} The page templates available in the current theme
+   */
+  static _getPageTemplates(jamboConfig) {
+    const defaultTheme = jamboConfig.defaultTheme;
+    const themesDir = jamboConfig.dirs && jamboConfig.dirs.themes;
+    if (!defaultTheme || !themesDir) {
+      return [];
+    }
+    const pageTemplatesDir = path.resolve(themesDir, defaultTheme, 'templates');
+    return fs.readdirSync(pageTemplatesDir);
+  }
+
+  /**
+   * Executes the add vertical command with the provided arguments.
+   * 
+   * @param {Object<string, string>} args The arguments, keyed by name 
+   */
+  execute(args) {
+    try {
+      this._createVerticalPage(args.name, args.template);
+      this._configureVerticalPage(args.name, args.verticalKey, args.cardName);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * Creates a page for the vertical using the provided name and template. If there is an
+   * error during page generation, an exception will be thrown.
+   * 
+   * @param {string} name The name of the vertical's page.
+   * @param {string} template The template to use.
+   */
+  _createVerticalPage(name, template) {
+    const args = ['--name', name, '--template', template];
+    const { stderr } = spawnSync('npx jambo page', args, { shell: true });
+    const pageCreationError = stderr && stderr.toString('utf-8');
+
+    if (pageCreationError) {
+      throw new Error(pageCreationError);
+    }
+  }
+
+  /**
+   * Updates the vertical page's configuration file. Specifically, placeholders for
+   * vertical key and card type are replaced with the provided values.
+   * 
+   * @param {string} name The page name.
+   * @param {string} verticalKey The vertical's key.
+   * @param {string} cardName The card to be used with the vertical.
+   */
+  _configureVerticalPage(name, verticalKey, cardName) {
+    const configFile = `config/${name}.json`;
+    let parsedConfig = fs.readFileSync(configFile, { encoding: 'utf-8' });
+    parsedConfig = parsedConfig.replace(/\<REPLACE ME\>/g, verticalKey);
+    parsedConfig = parsedConfig.replace(/"cardType": "[^,]+"/, `"cardType": "${cardName}"`);
+    fs.writeFileSync(configFile, parsedConfig);
+  }
+}
+module.exports = VerticalAdder;
