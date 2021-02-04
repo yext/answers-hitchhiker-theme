893a916b113ecb3cabe162800d65b7fd5426a3b7
Oliver Shi <oshi@yext.com>
2021-02-03 19:55:38 -0500

    jsdoc

--- static/js/get-injected-prop.js
+++ static/js/get-injected-prop.js
@@ -1,5 +1,20 @@
 import { isStaging } from './is-staging';
 
+/**
+ * Gets the prop specified by the propPath from the JAMBO_INJECTED_DATA,
+ * under the given experienceKey.
+ * 
+ * Will first choose the correct config, based on whether the browser
+ * is a STAGING or PRODUCTIon environment, under
+ * JAMBO_INJECTED_DATA.answers.experiences[experienceKey].configByLabel.
+ * Defaults to the top-level config at
+ * JAMBO_INJECTED_DATA.answers.experiences[experienceKey] if configByLabel
+ * does not exist.
+ * 
+ * @param {string} experienceKey 
+ * @param {Array<string>} propPath
+ * @returns {any}
+ */
 export function getInjectedProp(experienceKey, propPath) {
   const injectedData = JSON.parse(process.env.JAMBO_INJECTED_DATA || '{}');
   const experiences = injectedData?.answers?.experiences;
@@ -17,6 +32,14 @@ export function getInjectedProp(experienceKey, propPath) {
   return propAccumulator;
 }
 
+/**
+ * Chooses the STAGING or PRODUCTION config, defaulting to the top level config
+ * if configByLabel does not exist, or if the desired config does not exist.
+ *
+ * @param {Object} injectedData 
+ * @param {Object} experienceConfig
+ * @returns {Object}
+ */
 function getConfigForCurrentDomain(injectedData, experienceConfig) {
   const IS_STAGING = isStaging(injectedData?.pages?.domains);
   const { configByLabel } = experienceConfig;
