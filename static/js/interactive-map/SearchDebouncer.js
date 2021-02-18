1ea999b8f6aed3a3ce2c2589cc529bccef50b6d0
cea2aj <42848445+cea2aj@users.noreply.github.com>
2021-02-18 18:52:29 -0500

    Debounce searches on map movement (#611)
    Prevent searching twice when the map has only been moved a little
    
    The debouncer takes into account various zoom levels and only allows a search to be ran again once it is outside the distance threshold. The threshold is based on a percentage of the map width. This PR configures the debouncer to only allow a new search if the map has moved 20% of the map width away from the previous search.
    
    J=SLAP-1070
    TEST=manual
    
    Test the dragging the map around at various zoom levels and confirm that the map needs to move a certain amount before a new search is ran. Confirm the behavior is the same regardless of the zoom level. Test that a new search is ran when the zoom buttons are pressed.

---
+++ static/js/interactive-map/SearchDebouncer.js
@@ -0,0 +1,87 @@
+import { Coordinate } from './Geo/Coordinate.js';
+ 
+/**
+ * Responsible for determining whether or not a new search should be ran based on the
+ * location of the most recent search, the current center of the map, and the zoom level.
+ */
+class SearchDebouncer {
+  constructor () {
+    /**
+     * The threshold for allowing a new search based on the distance to the previous search.
+     * 
+     * The unit is approximately the map width percentage. For example, if the threshold is 50,
+     * a new search will not be allowed unless the currentMapCenter is further than 50% of the map
+     * width away from the previous search.
+     * 
+     * @type {number}
+     */
+    this.relativeDistanceThreshold = 20;
+
+    /**
+     * The threshold for allowing a new search based on a change in zoom.
+     * 
+     * With a zoom threshold of 1, a new search will be ran every time the zoom changes by 1 or greater.
+     * 
+     * @type {number}
+     */
+    this.zoomThreshold = 1;
+  }
+
+  /**
+   * Determines whether or not a search should be debounced
+   * 
+   * @param {Object} mostRecentSearchState
+   * @param {Coordinate} mostRecentSearchState.mapCenter
+   * @param {number} mostRecentSearchState.zoom
+   * @param {Object} currentMapState
+   * @param {Coordinate} currentMapState.mapCenter
+   * @param {number} currentMapState.zoom
+   * @retuns {boolean}
+   */
+  shouldBeDebounced (mostRecentSearchState, currentMapState) {
+    this._validateMapStateObject(mostRecentSearchState);
+    this._validateMapStateObject(currentMapState);
+
+    const distanceToLastSearch = mostRecentSearchState.mapCenter.distanceTo(currentMapState.mapCenter);
+
+    const relativeDistance = this._calculateRelativeDistance(distanceToLastSearch, currentMapState.zoom);
+    const zoomDifference = Math.abs(currentMapState.zoom - mostRecentSearchState.zoom)
+
+    const isOutsideDistanceThreshold = relativeDistance >= this.relativeDistanceThreshold;
+    const isOutsideZoomThreshold = zoomDifference >= this.zoomThreshold;
+
+    return !isOutsideDistanceThreshold && !isOutsideZoomThreshold;
+  }
+
+  /**
+   * Throws an error if the object is missing any of the required params
+   * 
+   * @param {Object}
+   */
+  _validateMapStateObject (obj) {
+    if (!('mapCenter' in obj)) {
+      throw new Error('The search debouncer was passed an object which is missing the "mapCenter" property');
+    }
+    if (!('zoom' in obj)) {
+      throw new Error('The search debouncer was passed an object which is missing the "zoom" property');
+    }
+  }
+
+  /**
+   * Calculates a distance relative to the map zoom level.
+   * 
+   * A relative distance of 100 is approximately equal to the width of the entire map.
+   * 
+   * Each change in the zoom level changes the total map width by an order of 2, which is why this formula
+   * uses `Math.pow()`.
+   * 
+   * @param {number} distance in miles
+   * @param {number} zoom
+   * @returns {number}
+   */
+  _calculateRelativeDistance (distance, zoom) {
+    return distance * Math.pow(2, zoom - 10);
+  }
+}
+
+export { SearchDebouncer };
