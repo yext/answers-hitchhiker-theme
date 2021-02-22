b9f9be957ccd88ffc612605299244b2975bbc2e8
Christian Reotutar <32468132+creotutar@users.noreply.github.com>
2021-02-22 01:16:09 -0500

    Collapsible Filters changes for Interactive Map (#614)
    * Collapsible Filters changes for Interactive Map
    
    We change collapsible filters in 3 ways for certain interactive map
    interactions:
    
    1. Add a check for when to scrollToTop when toggling collapsible filters
       in interactions. This manifests in the HTML as
       .js-CollapsibleFilters--disableScrollToTop. This is necessary because
       the results wrapper would scroll to top upon clicking a map pin
       attempting to scroll the results wrapper to the corresponding card.
    
    2. Add a top-level CollapsibleFilters--collapsed class that can be used
       to guide classes underneath. This was important to have collapsed
       filter styling in the interactive map (hide the LocationBias for
       example, only when the filters are collapsed).
    
    3. Move the order of the collapsible filters imports so that styles can
       be overridden in files that come after it in _default.scss (namely,
       InteractiveMap styling and answers.scss as a fun +1).
    
    J=SLAP-1093
    TEST=manual
    
    1. Test that you can pin on a result card and be scrolled direectly to
       the card, NOT to the top and then to the result card.
    
    2. Test that LocationBias and Search This Area disappear once the
       collapsible filters are expanded.
    
    3. See previous styling (100% width button, gray background results
       header) without !important in the css styles.
    
    * PR fixes: use configuration for scrollToTop, typos, double reverse logic
    
    * Stage typo fix

--- static/js/collapsible-filters/interactions.js
+++ static/js/collapsible-filters/interactions.js
@@ -2,18 +2,21 @@
  * Interactions manages page interactions for collapsible filters.
  */
 export default class Interactions {
-  constructor(domElements) {
-    const { filterEls, resultEls } = domElements;
+  constructor(config) {
+    const { filterEls, resultEls, disableScrollToTopOnToggle } = config;
+    this.collapsibleFiltersParentEl = document.querySelector('.CollapsibleFilters');
     this.filterEls = filterEls || [];
     this.resultEls = resultEls || [];
     this.viewResultsButton = document.getElementById('js-answersViewResultsButton');
     this.searchBarContainer = document.getElementById('js-answersSearchBar');
     this.resultsColumn = document.querySelector('.js-answersResultsColumn');
     this.inactiveCssClass = 'CollapsibleFilters-inactive';
+    this.collapsedcCssClass = 'CollapsibleFilters--collapsed';
     this.resultsWrapper = document.querySelector('.js-answersResultsWrapper')
       || document.querySelector('.Answers-resultsWrapper');
     this._updateStickyButton = this._updateStickyButton.bind(this);
     this._debouncedStickyUpdate = this._debouncedStickyUpdate.bind(this);
+    this._disableScrollToTopOnToggle = disableScrollToTopOnToggle;
   }
 
   /**
@@ -170,6 +173,20 @@ export default class Interactions {
   }
 
   /**
+   * If isCollapsed is true, then set the css class for CollapsibleFilters 
+   * indicating that the filters are collapsed.
+   *
+   * @param {boolean} isCollapsed
+   */
+  toggleCollapsedStatusClass(isCollapsed) {
+    if (isCollapsed) {
+      this.collapsibleFiltersParentEl.classList.add(this.collapsedcCssClass)
+    } else {
+      this.collapsibleFiltersParentEl.classList.remove(this.collapsedcCssClass);
+    }
+  }
+
+  /**
    * Either collapses or expands the collapsible filters panel.
    * @param {boolean} shouldCollapseFilters 
    */
@@ -186,7 +203,10 @@ export default class Interactions {
       this.toggleInactiveClass(el, !shouldCollapseFilters);
     }
     this.toggleInactiveClass(this.viewResultsButton, shouldCollapseFilters);
-    this.scrollToTop();
+    this.toggleCollapsedStatusClass(shouldCollapseFilters);
+    if (!this._disableScrollToTopOnToggle) {
+      this.scrollToTop();
+    }
     ANSWERS.components.getActiveComponent('FilterLink').setState({
       panelIsDisplayed: !shouldCollapseFilters
     });
