9aee96929803d4ed1a2b5ffed7863113070d57c2
creotutar <creotutar@yext.com>
2021-02-23 15:11:19 -0500

    Re-add card click storage key in rebased partial

--- static/js/interactive-map/location-card/add-card-click-listener.js
+++ static/js/interactive-map/location-card/add-card-click-listener.js
@@ -1,6 +1,6 @@
 this._container.parentElement.addEventListener('click', () => {
   const { _index } = JSON.parse(this._container.parentElement.dataset.opts || {});
-  this.core.globalStorage.set('card-click', { index: _index });
+  this.core.storage.set(HitchhikerJS.StorageKeys.LOCATOR_CARD_CLICK, { index: _index });
 
   document.querySelectorAll('.yxt-Card--pinClicked').forEach((el) => {
     el.classList.remove('yxt-Card--pinClicked');
