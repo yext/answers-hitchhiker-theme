a22638dca0e5cfa0a542bc1543e2af6493dc9514
Connor Anderson <canderson@yext.com>
2021-03-23 11:39:43 -0400

    jsdoc

--- hbshelpers/codegen/TransformFacetsCall.js
+++ hbshelpers/codegen/TransformFacetsCall.js
@@ -1,9 +1,14 @@
 /**
- * Responsible for generating transformFacets function calls based on the fields config option
+ * Responsible for generating transformFacets function calls
  */
 class TransformFacetsCall {
+  /**
+   * Generates a transformFacets function based on the fields config
+   * 
+   * @param {Object} fields filterOptions keyed by fieldId
+   */
   static generate (fields) {
-    return `facets => {
+    return `(facets, config) => {
       return facets.map(facet => {
         let options = facet.options;
         ${this._generateFacetModificationCode(fields)}
@@ -12,23 +17,39 @@ class TransformFacetsCall {
     }`
   }
 
+  /**
+   * Generates code which modifies the in-scope facet based on the fields config
+   * 
+   * @param {Object} fields filterOptions keyed by fieldId
+   */
   static _generateFacetModificationCode (fields) {
-    return Object.entries(fields).reduce((acc, [fieldId, fieldOptions]) => {
+    return Object.entries(fields).reduce((acc, [fieldId, filterOptions]) => {
       return acc + `if (facet.fieldId === '${fieldId}') {
-        ${this._generateFilterOptionsCode(fieldOptions)}
-        ${this._generateFacetOptionsModificationCode(fieldOptions.fieldLabels)}
+        ${this._generateFilterOptionsSetterCode(filterOptions)}
+        ${this._generateFacetOptionsModificationCode(filterOptions.fieldLabels)}
       }\n`;
     }, '');
   }
 
-  static _generateFilterOptionsCode (fieldOptions) {
-    return Object.entries(fieldOptions).reduce((acc, [option, value]) => {
+  /**
+   * Generates code which sets filterOptions on the in-scope facet. The fieldLabels
+   * option is not included because it is processed separately.
+   * 
+   * @param {Object} filterOptions option values keyed by option name
+   */
+  static _generateFilterOptionsSetterCode (filterOptions) {
+    return Object.entries(filterOptions).reduce((acc, [option, value]) => {
       return option !== 'fieldLabels'
         ? acc + `facet['${option}'] = '${value}';\n`
         : acc;
     }, '');
   }
 
+  /**
+   * Generates code which modifies the display names of the in-scope DisplayableFacetOption array
+   * 
+   * @param {Object} fieldLabels new option display names keyed by old display names
+   */
   static _generateFacetOptionsModificationCode (fieldLabels) {
     if (!fieldLabels) {
       return '';
