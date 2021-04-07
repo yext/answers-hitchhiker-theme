a5d2d3f35696b13ae4b5391f4c4051736ba4b485
Oliver Shi <oshi@yext.com>
2021-04-07 12:11:36 -0400

    missed a new file

---
+++ static/js/constants/results-context.js
@@ -0,0 +1,11 @@
+/**
+ * ResultsContext is an ENUM that provides context
+ * for the results that we are storing from server
+ * data
+ * @enum {string}
+ */
+const ResultsContext = {
+  NORMAL: 'normal',
+  NO_RESULTS: 'no-results'
+};
+export default ResultsContext;
