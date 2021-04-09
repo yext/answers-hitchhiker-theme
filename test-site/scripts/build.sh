b10faa71ba0ed2de2294bd5c9ff8c77bc55b9fd1
Connor Anderson <canderson@yext.com>
2021-04-09 11:40:47 -0400

    set -e

--- test-site/scripts/build.sh
+++ test-site/scripts/build.sh
@@ -10,6 +10,8 @@ set_working_dir_to_test_site
 rm -rf cards/event-custom
 rm -rf directanswercards/allfields-custom
 
+set -e
+
 npx jambo card --name event-custom --templateCardFolder cards/event-standard
 npx jambo directanswercard --name allfields-custom --templateCardFolder directanswercards/allfields-standard
 
