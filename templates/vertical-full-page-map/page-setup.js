2ff813ec81bf0babd1a466f36eff23d63bd36d73
cea2aj <42848445+cea2aj@users.noreply.github.com>
2021-04-26 11:23:28 -0400

    Fix locator bundle race condition (#739)
    Fix a race condition where the vertical-full-page-map cards would break if the search loads before the locator bundle
    
    J=SLAP-1276
    TEST=manual
    
    Use Fiddler to delay the loading of the locator bundle and observe the cards rendering properly. The cards aren't clickable while the bundle is loading, but as soon as it does, the cards become clickable. Also test that when the bundle loads before the search, the listeners register properly.

--- templates/vertical-full-page-map/page-setup.js
+++ templates/vertical-full-page-map/page-setup.js
@@ -7,5 +7,25 @@ if (window.locatorBundleLoaded) {
   loadFullPageMap();
 } else {
   const locatorBundleScript = document.querySelector('script#js-answersLocatorBundleScript');
-  locatorBundleScript.onload = loadFullPageMap;
+  locatorBundleScript.onload = () => {
+    window.locatorBundleLoaded = true;
+    locatorBundleScript.dispatchEvent(new Event('locator-bundle-loaded'));
+    loadFullPageMap();
+  }
+}
+
+/**
+ * Registers listeners on the card once the locator bundle is loaded
+ *
+ * @param {ANSWERS.Component} card A location card
+ */
+function registerVerticalFullPageMapCardListeners(card) {
+  if (window.locatorBundleLoaded) {
+    new VerticalFullPageMap.CardListenerAssigner({card: card}).addListenersToCard();
+    return;
+  }
+  const locatorBundleScript = document.querySelector('script#js-answersLocatorBundleScript');
+  locatorBundleScript.addEventListener('locator-bundle-loaded', () => {
+    new VerticalFullPageMap.CardListenerAssigner({card: card}).addListenersToCard();
+  });
 }
