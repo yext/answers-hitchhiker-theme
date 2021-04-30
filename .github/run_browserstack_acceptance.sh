076d26df54e412ea68133459a3e819e23fb68092
Connor Anderson <canderson@yext.com>
2021-04-30 11:16:10 -0400

    don't use automate 1

--- .github/run_browserstack_acceptance.sh
+++ .github/run_browserstack_acceptance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export BROWSERSTACK_USE_AUTOMATE="1"
+#export BROWSERSTACK_USE_AUTOMATE="1"
 export BROWSERSTACK_PROJECT_NAME="Answers Hitchhiker Theme"
 GITHUB_BRANCH=${GITHUB_REF#refs/heads/}
 export BROWSERSTACK_BUILD_ID="${GITHUB_BRANCH} - ${GITHUB_JOB}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
 
-npx testcafe "browserstack:ie@11.0,browserstack:safari" tests/acceptance/suites/*.js -q
+npx testcafe "browserstack:safari" tests/acceptance/suites/*.js -q
