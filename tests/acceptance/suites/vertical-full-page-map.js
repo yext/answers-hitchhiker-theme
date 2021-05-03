fe8c51a15e086f0ffea3e6311d17a8f5b86a0cca
cea2aj <42848445+cea2aj@users.noreply.github.com>
2021-05-03 15:30:20 -0400

    Setup Acceptance Test Infrastructure (#749)
    Setup infrastructure for running browserstack tests on the theme test site
    
    J=SLAP-1268
    TEST=acceptance
    
    Confirm that the tests run in browswerstack. Observe the browserstack video and see that the test site loads
    
    Co-authored-by: Oliver Shi <oshi@yext.com>

---
+++ tests/acceptance/suites/vertical-full-page-map.js
@@ -0,0 +1,8 @@
+const { PORT } = require('../constants');
+
+fixture`Vertical Full Page Map`
+  .page(`http://localhost:${PORT}/locations_full_page_map`)
+
+test('The test runs', async () => {
+
+})
