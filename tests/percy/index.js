88fc1deb0316f9e3c797ce08711e21171a1e5b86
Oliver Shi <oshi@yext.com>
2021-05-06 11:07:23 -0400

    add back serve-handler

--- tests/percy/index.js
+++ tests/percy/index.js
@@ -5,14 +5,23 @@ const StandardPageNavigator = require('./standardpagenavigator');
 const IframePageNavigator = require('./iframepagenavigator');
 const Camera = require('./camera');
 
-const PORT = 4000;
+const PORT = 5042;
 
 PercyScript.run(async (page, percySnapshot) => {
+  const server = new HttpServer({
+    dir: 'test-site/public',
+    port: PORT
+  })
+
+  server.start();
+
   // const standardPageNavigator = new StandardPageNavigator(page, `http://localhost:${PORT}`);
   const iframePageNavigator = new IframePageNavigator(page, `http://localhost:${PORT}`, 'iframe_test');
-  const standardCamera = new Camera(percySnapshot);
+  // const standardCamera = new Camera(percySnapshot);
   const iframeCamera = new Camera(percySnapshot, true);
 
   // await (new Photographer(standardPageNavigator, standardCamera).captureSnapshots());
   await (new Photographer(iframePageNavigator, iframeCamera).captureSnapshots());
+
+  server.shutdown();
 });
