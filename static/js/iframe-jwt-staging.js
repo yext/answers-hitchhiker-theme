5b0d0ffc1666f01c013a7ced8491fb27ae014502
Connor Anderson <canderson@yext.com>
2021-05-07 13:58:58 -0400

    messaging updates

--- static/js/iframe-jwt-staging.js
+++ static/js/iframe-jwt-staging.js
@@ -1,8 +1,11 @@
 import { generateIFrame } from './iframe-common';
 import InjectedData from './models/InjectedData';
+import getJwtNotProvidedTimeout from './utils/getJwtNotProvidedTimeout';
 
 const stagingDomain = new InjectedData().getStagingDomain();
+const jwtNotProvidedTimeout = getJwtNotProvidedTimeout();
 
 window.initAnswersFrameJWT = function (token) {
+  clearTimeout(jwtNotProvidedTimeout);
   generateIFrame(stagingDomain, null, null, token);
 }
