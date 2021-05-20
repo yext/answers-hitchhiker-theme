6d554334872de56704d3dcfc782dc823e967e125
Connor Anderson <canderson@yext.com>
2021-05-20 15:29:50 -0400

    fix unit tests

--- tests/script/partials/sdk-js-script-tags.js
+++ tests/script/partials/sdk-js-script-tags.js
@@ -3,24 +3,24 @@ const hbs = require('../../test-utils/hbs');
 const defaultOutput = `<script
   src="https://assets.sitescdn.net/answers/v1.8/answers-modern.min.js"
   type="module"
-  onload="initAnswers()"
+  onload="onAnswersScriptLoad()"
 ></script>
 <script
   src="https://assets.sitescdn.net/answers/v1.8/answers.min.js"
   nomodule
-  onload="initAnswers()"
+  onload="onAnswersScriptLoad()"
   defer
 ></script>`;
 
 const jaOutput = `<script
   src="https://assets.sitescdn.net/answers/v1.8/ja-answers-modern.min.js"
   type="module"
-  onload="initAnswers()"
+  onload="onAnswersScriptLoad()"
 ></script>
 <script
   src="https://assets.sitescdn.net/answers/v1.8/ja-answers.min.js"
   nomodule
-  onload="initAnswers()"
+  onload="onAnswersScriptLoad()"
   defer
 ></script>`;
 
