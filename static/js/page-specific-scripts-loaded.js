520888a7a64bfd5e935d1a8a51b9bbd3cfb2124d
Connor Anderson <canderson@yext.com>
2021-05-21 12:01:12 -0400

    tweak on error behavior

--- static/js/page-specific-scripts-loaded.js
+++ static/js/page-specific-scripts-loaded.js
@@ -1,12 +1,16 @@
 /**
  * A promise executor function which calls resolve once all page-specific scripts are loaded
  *
- * @param {Function} resolve 
+ * @param {Function} resolve
+ * @param {Function} reject
  */
-export default function (resolve) {
+export default function (resolve, reject) {
   const locatorBundleScript = document.querySelector('script#js-answersLocatorBundleScript');
   if (locatorBundleScript) {
     locatorBundleScript.onload = resolve;
+    locatorBundleScript.onerror = () => {
+      reject('Failed to load locator-bundle.js');
+    };
   } else {
     resolve();
   }
