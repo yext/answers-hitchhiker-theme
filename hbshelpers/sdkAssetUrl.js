4b849730d7ff0ce8b49f48b5d2eef450f161b095
Tom Meyer <tmeyer@yext.com>
2021-05-24 09:08:03 -0400

    Updates.

--- hbshelpers/sdkAssetUrl.js
+++ hbshelpers/sdkAssetUrl.js
@@ -2,16 +2,13 @@ const RELEASE_BRANCH_REGEX = /^release\/v[0-9.]+$/;
 const SEM_VER_REGEX = /^[1-9]+$|^[1-9]+\.[0-9]+$|^[1-9]+\.[0-9]+\.[0-9]+$/;
 
 module.exports = function sdkAssetUrl(branch, locale, assetName) {
-  console.log(branch);
-  console.log(locale);
-  console.log(assetName);
   const isReleasedBranch = SEM_VER_REGEX.test(branch);
 
   let parsedBranch;
   if (isReleasedBranch) {
     parsedBranch = `v${branch}`;
   } else {
-    branchPrefix = `dev/${branchPrefix.replace(/\//g, '-')}`;
+    parsedBranch = `dev/${branch.replace(/\//g, '-')}`;
   }
 
   const isPreReleaseBranch = RELEASE_BRANCH_REGEX.test(branch);
