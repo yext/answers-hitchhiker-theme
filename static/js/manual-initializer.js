b8dd5a456c53cc8ead16198249bc84cdb25f2eda
Connor Anderson <canderson@yext.com>
2021-05-26 15:34:43 -0400

    feedback

--- static/js/manual-initializer.js
+++ static/js/manual-initializer.js
@@ -4,38 +4,35 @@ import updateRuntimeConfig from './utils/update-runtime-config';
  * Responsible for setting up manual initialization of the experience
  */
 export default class ManualInitializer {
-  constructor () {
+  /**
+   * @param {Function} initAnswers the Answers initialization function which the manual init function calls to start answers
+   */
+  constructor (initAnswers) {
+    this._initAnswers = initAnswers;
     this._hasAnswersInitialized = false;
   }
 
   /**
-   * Puts a manual initializaiton function on the window
+   * Puts a manual initialization function on the window which initializes answers with the
+   * provided runtimeConfig
    */
   setup () {
-    window.initAnswersExperience = this._generateInitFunction();
-  }
-
-  /**
-   * Returns a function which initializes an answers experience with the provided runtimeConfig
-   * @returns {Function}
-   */
-  _generateInitFunction () {
     const runtimeConfigNotProvidedTimeout = setTimeout(() => {
       console.warn(
         'An initialization function has not been called within 5 seconds of page load, and "initializeManually" is set to true.\n' +
         'Load the experience by calling initAnswersExperience() or initAnswersExperienceFrame() for iframe integrations.'
       );
     }, 5000);
-    return runtimeConfig => {
+    window.initAnswersExperience = (config) => {
       this._fireOnDomLoad(() => {
-        updateRuntimeConfig(runtimeConfig);
+        updateRuntimeConfig(config);
         clearTimeout(runtimeConfigNotProvidedTimeout);
         if (!this._hasAnswersInitialized) {
-          initAnswers();
+          this._initAnswers();
         }
         this._hasAnswersInitialized = true;
       });
-    };
+    }
   }
 
   /**
