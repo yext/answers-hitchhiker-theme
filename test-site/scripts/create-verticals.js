44c39d1b23227b01784973f76e2ef6f883ce564e
Oliver Shi <oshi@yext.com>
2021-05-28 11:02:15 -0400

    fix google streetview man being invisible (#801)
    Mr. Pegman was being covered up due to our css
    reset setting his max-height to 100%, which happened
    to be 0 due to how the google maps js works.
    
    This commit adds a css override on top of that.
    I wanted to remove the actual line of the css reset
    causing this behavior, however it's likely that removing
    that line would cause ripples throughout the html-css continuum.
    
    T=409037
    TEST=manual, auto
    
    see mr. pegman again
    add a percy snapshot for him

--- test-site/scripts/create-verticals.js
+++ test-site/scripts/create-verticals.js
@@ -20,6 +20,11 @@ const verticalConfiguration = {
     template: 'vertical-map',
     cardName: 'location-standard'
   },
+  locations_google: {
+    verticalKey: 'KM',
+    template: 'vertical-map',
+    cardName: 'location-standard'
+  },
   locations_full_page_map: {
     verticalKey: 'KM',
     template: 'vertical-full-page-map',
