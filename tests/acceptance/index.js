86cfe45ee4afb640987dc823ebc3a280c9923512
cea2aj <42848445+cea2aj@users.noreply.github.com>
2021-06-23 14:03:09 -0400

    Testcafe config file (#849)
    Add a testcafe config file to make the internet explorer tests more robust
    
    J=none
    TEST=manual
    
    Run the tests and confirm they pass

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -21,7 +21,9 @@ runTests(argv.browsers);
  * @param {string[]} browsers The browsers to run the tests on
  */
 async function runTests (browsers) {
-  const testcafe = await createTestCafe();
+  const testcafe = await createTestCafe({
+    configFile: './testcafe.json'
+  });
   try {
     const numberTestsFailed = await testcafe.createRunner()
       .src('tests/acceptance/suites/*.js')
