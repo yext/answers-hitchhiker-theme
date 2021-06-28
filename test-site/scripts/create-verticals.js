fb837cd147c51980eb163ca9398cd00c8a91b070
Oliver Shi <oshi@yext.com>
2021-06-28 18:44:51 -0400

    dont build other pages

--- test-site/scripts/create-verticals.js
+++ test-site/scripts/create-verticals.js
@@ -5,75 +5,10 @@ const ConfigMerger = require('./config-merger');
 const PagePatcher = require('./page-patcher');
 
 const verticalConfiguration = {
-  events: {
-    verticalKey: 'events',
-    template: 'vertical-standard',
-    cardName: 'event-custom'
-  },
-  faqs: {
-    verticalKey: 'faq',
-    template: 'vertical-standard',
-    cardName: 'faq-accordion'
-  },
-  locations: {
-    verticalKey: 'KM',
-    template: 'vertical-map',
-    cardName: 'location-standard'
-  },
-  locations_google: {
-    verticalKey: 'KM',
-    template: 'vertical-map',
-    cardName: 'location-standard'
-  },
   locations_full_page_map: {
     verticalKey: 'KM',
     template: 'vertical-full-page-map',
     cardName: 'location-standard'
-  },
-  locations_full_page_map_with_filters: {
-    verticalKey: 'KM',
-    template: 'vertical-full-page-map',
-    cardName: 'location-standard'
-  },
-  people: {
-    verticalKey: 'people',
-    template: 'vertical-grid',
-    cardName: 'standard'
-  },
-  products: {
-    verticalKey: 'products',
-    template: 'vertical-grid',
-    cardName: 'multilang-product-prominentvideo'
-  },
-  products_clickable_image: {
-    verticalKey: 'products',
-    template: 'vertical-grid',
-    cardName: 'product-prominentimage-clickable'
-  },
-  financial_professionals: {
-    verticalKey: 'financial_professionals',
-    template: 'vertical-standard',
-    cardName: 'financial-professional-location'
-  },
-  healthcare_professionals: {
-    verticalKey: 'healthcare_professionals',
-    template: 'vertical-grid',
-    cardName: 'professional-standard'
-  },
-  jobs: {
-    verticalKey: 'jobs',
-    template: 'vertical-standard',
-    cardName: 'job-standard'
-  },
-  help_articles: {
-    verticalKey: 'help_articles',
-    template: 'vertical-standard',
-    cardName: 'document-standard'
-  },
-  menu_items: {
-    verticalKey: 'menu_items',
-    template: 'vertical-grid',
-    cardName: 'menuitem-standard'
   }
 };
 
