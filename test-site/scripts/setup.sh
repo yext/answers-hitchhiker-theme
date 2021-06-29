79b05a2d270c3e3126d34c29f41c62ec8702891f
Connor Anderson <canderson@yext.com>
2021-06-28 22:33:49 -0400

    rework scripts

--- test-site/scripts/setup.sh
+++ test-site/scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 set_working_dir_to_test_site () {
   path_to_this_script="$( dirname "${BASH_SOURCE[0]}" )"
   cd "$path_to_this_script/.."
@@ -12,6 +14,19 @@ copy_static_files_into_working_dir () {
   cp ../static/webpack-config.js webpack-config.js
 }
 
+cleanup_custom_cards () {
+  rm -rf cards/event-custom
+  rm -rf directanswercards/allfields-custom
+}
+
+create_custom_cards () {
+  npx jambo card --name event-custom --templateCardFolder cards/event-standard
+  npx jambo directanswercard --name allfields-custom --templateCardFolder directanswercards/allfields-standard
+}
+
 set_working_dir_to_test_site
 copy_static_files_into_working_dir
 npm i
+cleanup_custom_cards
+create_custom_cards
+node scripts/create-verticals.js
