ba7cd3608e5b6ab08b96b589e6c6e4146534388b
Christopher Jiang <cjiang@yext.com>
2021-07-16 17:21:17 -0400

    remove console log

--- hbshelpers/assign.js
+++ hbshelpers/assign.js
@@ -1,6 +1,5 @@
 module.exports = function assign(...options) {
   var parsedData = JSON.parse(options[options.length - 1].fn(this));
   var stringFormat = JSON.stringify(Object.assign({},...parsedData));
-  console.log(stringFormat)
   return stringFormat
 }
