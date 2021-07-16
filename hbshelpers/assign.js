1e738cdcf0a7508cfc34c5a4d74a89596eb0b82a
Christopher Jiang <cjiang@yext.com>
2021-07-16 17:16:02 -0400

    remove extra {}

--- hbshelpers/assign.js
+++ hbshelpers/assign.js
@@ -1,6 +1,6 @@
 module.exports = function assign(...options) {
   var parsedData = JSON.parse(options[options.length - 1].fn(this));
   var stringFormat = JSON.stringify(Object.assign({},...parsedData));
-  stringFormat.replace('"', '');
+  console.log(stringFormat)
   return stringFormat
 }
