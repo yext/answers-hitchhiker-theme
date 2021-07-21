190dd7c5ac86f42c7d98e42be49aa1b80b7cfa13
Yen Truong <ytruong@yext.com>
2021-07-21 13:14:26 -0400

    test multiple testcafe instance

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -13,23 +13,57 @@ const argv = yargs
   .alias('help', 'h')
   .parse();
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function serveSite() {
+  await exec(`npx serve -l tcp://0.0.0.0:${PORT} test-site/public`);
+}
+
+serveSite();
 runTests(argv.browsers);
+
+// runSampleTest(argv.browsers);
+// console.log('out');
+// async function runSampleTest (browsers) {
+//   const testcafe = await createTestCafe();
+//   try {
+//   const runner = testcafe.createRunner();
+//   const failedCount = await runner
+//     .src('tests/acceptance/suites/vertical-full-page-map.js')
+//     .browsers(browsers)
+//     .concurrency(2)
+//     .run();
+//     console.log('Tests failed: ' + failedCount);
+//   }
+//   finally {
+//       console.log('closing..');
+//       await testcafe.close();
+//       console.log('closed.');
+//   }
+//   console.log('done');
+// }
 
 /**
  * Run the acceptance tests
  * 
  * @param {string[]} browsers The browsers to run the tests on
  */
-async function runTests (browsers) {
+function runTests (browsers) {
+  const tests = [];
+  browsers.forEach(browser => tests.push(testCafeInstance(browser)));
+  Promise.all(tests).then(() => {console.log("Completed all testcafe acceptance tests.");});
+}
+
+async function testCafeInstance(browser) {
   const testcafe = await createTestCafe({
     configFile: './testcafe.json'
   });
   try {
     const numberTestsFailed = await testcafe.createRunner()
       .src('tests/acceptance/suites/*.js')
-      .browsers(browsers)
+      .browsers(browser)
       .concurrency(2)
-      .startApp(`npx serve -l tcp://0.0.0.0:${PORT} test-site/public`, 4000)
       .run({ quarantineMode: true });
     if (numberTestsFailed > 0) {
       await testcafe.close();
