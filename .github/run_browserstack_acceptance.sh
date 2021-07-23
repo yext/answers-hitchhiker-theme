cf61ff4fce9478fce4914d3e9f1fa32976db4548
Yen Truong <ytruong@yext.com>
2021-07-23 10:48:28 -0400

    test concurrency on hotfix branch

--- .github/run_browserstack_acceptance.sh
+++ .github/run_browserstack_acceptance.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 export BROWSERSTACK_USE_AUTOMATE="1"
 export BROWSERSTACK_PROJECT_NAME="Answers Hitchhiker Theme"
-GITHUB_BRANCH=${GITHUB_REF#refs/heads/}
+# GITHUB_BRANCH=${GITHUB_REF#refs/heads/}
+GITHUB_BRANCH="hotfix/fake-branch"
 export BROWSERSTACK_BUILD_ID="${GITHUB_BRANCH} - ${GITHUB_RUN_ID}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
 
-npm run acceptance -- --browsers browserstack:ie@11.0 browserstack:safari browserstack:firefox
+if [[ $GITHUB_BRANCH == release/*
+  || $GITHUB_BRANCH == hotfix/*
+  || $GITHUB_BRANCH == master
+  || $GITHUB_BRANCH == support* ]]
+then
+  npm run acceptance -- --browsers browserstack:ie@11.0 browserstack:safari browserstack:firefox
+else
+  npm run acceptance -- --browsers browserstack:ie@11.0 --concurrency 2
+fi
