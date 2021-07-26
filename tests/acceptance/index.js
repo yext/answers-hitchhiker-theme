2ec77676c163029f8140bb0e1522edb618630b08
Yen Truong <36055303+yen-tt@users.noreply.github.com>
2021-07-26 13:58:45 -0400

    TestCafe: concurrency to acceptance tests (#890)
    - update testcafe runner configuration to opens 2 instances of the same browser
    - update run_browserstack_acceptance script to spin 1 instance of all three browser types on feature, hotfix, master, and support branch. All other branches will spin up 2 instances of ie11.
    
    J=1431
    TEST=auto
    
    - ran acceptance tests from circleCI workflow multiple times. See that 2 sessions of each browser type is setup in browserStack. See that the time, for the most part, is reduced by 2-3 mins.
    - tested script locally: change the GITHUB_BRANCH var to different branch names and see what command line is called
    - tested script through circleCI from pr: made a push to pr with a hardcoded GITHUB_BRANCH value (hotfix/test-something) and see that it spin up 2 ie11 instance in browserstack. made a push without hardcoded value (which will take this pr name dev/testcafe-concurrency) and see that 6 instances (2 of each browser type) is up in browserstack.

--- tests/acceptance/index.js
+++ tests/acceptance/index.js
@@ -3,24 +3,33 @@ const { PORT } = require('./constants');
 const yargs = require('yargs');
 
 const argv = yargs
-  .option('browsers', {
-    description: 'The browsers to run the tests on',
-    alias: 'b',
-    type: 'array',
-    default: 'chrome'
+  .options({
+    'browsers': {
+      description: 'The browsers to run the tests on',
+      alias: 'b',
+      type: 'array',
+      default: 'chrome'
+    },
+    'concurrency': {
+      description: 'Number of instances of each browser type',
+      alias: 'c',
+      type: 'number',
+      default: 1
+    }
   })
   .help()
   .alias('help', 'h')
   .parse();
 
-runTests(argv.browsers);
+runTests(argv.browsers, argv.concurrency);
 
 /**
  * Run the acceptance tests
  * 
  * @param {string[]} browsers The browsers to run the tests on
+ * @param {number} concurrency Number of instances of each browser type
  */
-async function runTests (browsers) {
+async function runTests (browsers, concurrency) {
   const testcafe = await createTestCafe({
     configFile: './testcafe.json'
   });
@@ -28,6 +37,7 @@ async function runTests (browsers) {
     const numberTestsFailed = await testcafe.createRunner()
       .src('tests/acceptance/suites/*.js')
       .browsers(browsers)
+      .concurrency(concurrency)
       .startApp(`npx serve -l tcp://0.0.0.0:${PORT} test-site/public`, 4000)
       .run({ quarantineMode: true });
     if (numberTestsFailed > 0) {
