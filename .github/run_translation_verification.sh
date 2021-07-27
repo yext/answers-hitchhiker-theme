75cd5396b6ab49ca362043c493379d5694af1c18
Connor Anderson <canderson@yext.com>
2021-07-27 11:22:09 -0400

    update scripts

---
+++ .github/run_translation_verification.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+# Verifiy that the messages.pot file in the repo is up to date
+# If there are any git diffs after translations are extracted, the checked-in file is out of date
+npm run extract-translations
+
+git diff --exit-code translations/messages.pot > /dev/null # send stdout to /dev/null to reduce clutter in the CI output
+diff_exit_code=$?
+
+if test $diff_exit_code -eq 1
+then
+  echo "Extracted translations are out of date. Run 'npm run extract-translations' and commit the updated pot file."
+  exit 1
+fi
+
+cd translations
+
+# Verify that all translations are present
+for po_file in *.po
+  do msgcmp $po_file messages.pot
+done
