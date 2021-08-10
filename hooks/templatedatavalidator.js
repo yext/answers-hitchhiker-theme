13e0d3f27ec000e200d60a2053b5dbcb51dba33c
Oliver Shi <oshi@yext.com>
2021-08-10 18:31:38 -0400

    use partials in validator
    Use the new partials param in templatedatavalidator instead of fs.existsSync.
    This is slightly more robust, for weird cases when somebody registers a card
    or universalsectiontemplate in an atypical folder.
    
    J=SLAP-1489
    TEST=manual
    
    test that the validator will catch incorrect card and unviersalsectiontemplates

--- hooks/templatedatavalidator.js
+++ hooks/templatedatavalidator.js
@@ -1,18 +1,21 @@
-var fs = require('fs');
-var path = require('path');
+const path = require('path');
 const { parseJamboConfig } = require('../commands/helpers/utils/jamboconfigutils');
 const { error } = require('../commands/helpers/utils/logger');
+
 /**
  * Validates page template's data configuration
  * 
  * @param {Object} pageData data that gets passed into a page template
+ * @param {Object<string, Function|string>} partials
+ *                          mapping of partial name to partial. Handlebars
+ *                          converts partials from strings to Functions when they are used.
  * @returns {boolean} false if validator should throw an error
  */
-module.exports = function (pageData) {
+module.exports = function (pageData, partials) {
   const jamboConfig = parseJamboConfig();
   const validatorResults = [
     isGlobalConfigValid(pageData.global_config), 
-    isPageVerticalConfigValid(pageData, jamboConfig)
+    isPageVerticalConfigValid(pageData, jamboConfig, partials)
   ];
   const isValid = validatorResults.every(result => result);
   return isValid;
@@ -37,20 +40,21 @@ function isGlobalConfigValid(globalConfig) {
  * 
  * @param {Object} pageData 
  * @param {Object} jamboConfig 
+ * @param {Object<string, Function|string>} partials mapping of partial name to partial
  * @returns {boolean}
  */
-function isPageVerticalConfigValid(pageData, jamboConfig) {
+function isPageVerticalConfigValid(pageData, jamboConfig, partials) {
   const themeDirectory = path.join(jamboConfig.dirs.themes, jamboConfig.defaultTheme);
   return Object.keys(pageData.verticalsToConfig)
     .map(key => {
       if (key === 'Universal') {
-        return isAllVerticalConfigsValid(pageData.verticalConfigs, jamboConfig);
+        return isAllVerticalConfigsValid(pageData.verticalConfigs, jamboConfig, partials);
       }
       const universalSectionTemplate = pageData.verticalsToConfig[key].universalSectionTemplate;
       const cardType = pageData.verticalsToConfig[key].cardType;
       const validatorResults = [
-        isUniversalSectionTemplateValid(key, themeDirectory, universalSectionTemplate), 
-        isCardTypeValid(key, themeDirectory, cardType)
+        isUniversalSectionTemplateValid(key, themeDirectory, universalSectionTemplate, partials), 
+        isCardTypeValid(key, themeDirectory, cardType, partials)
       ];
       return validatorResults.every(result => result);
     })
@@ -63,15 +67,14 @@ function isPageVerticalConfigValid(pageData, jamboConfig) {
  * @param {string} verticalName
  * @param {string} themeDir
  * @param {string} template
+ * @param {Object<string, Function|string>} partials mapping of partial name to partial
  * @returns {boolean}
  */
-function isUniversalSectionTemplateValid(verticalName, themeDir, template) {
-  if (template) {
+function isUniversalSectionTemplateValid(verticalName, themeDir, template, partials) {
+  if (template && !partials[`universalsectiontemplates/${template}`]) {
     const universalSectionPath = path.join(themeDir, 'universalsectiontemplates/', template + '.hbs');
-    if (!fs.existsSync(universalSectionPath)) {
-      error(`Invalid universalSectionTemplate: can't find "${template}" at the expected path "${universalSectionPath}" for vertical "${verticalName}".`);
-      return false;
-    }
+    error(`Invalid universalSectionTemplate: can't find "${template}" at the expected path "${universalSectionPath}" for vertical "${verticalName}".`);
+    return false;
   }
   return true;
 }
@@ -82,16 +85,15 @@ function isUniversalSectionTemplateValid(verticalName, themeDir, template) {
  * @param {string} verticalName
  * @param {string} themeDir
  * @param {string} cardType
+ * @param {Object<string, Function|string>} partials mapping of partial name to partial
  * @returns {boolean}
  */
-function isCardTypeValid(verticalName, themeDir, cardType) {
-  if (cardType) {
+function isCardTypeValid(verticalName, themeDir, cardType, partials) {
+  if (cardType && !partials[`cards/${cardType}/component`]) {
     const cardTypePath = path.join(themeDir, 'cards/', cardType);
     const customCardTypePath = path.join('cards/', cardType);
-    if (!fs.existsSync(cardTypePath) && !fs.existsSync(customCardTypePath)) {
-      error(`Invalid cardType: can't find "${cardType}" at at the expected paths "${cardTypePath}" or "${customCardTypePath}" for vertical "${verticalName}".`);
-      return false;
-    }
+    error(`Invalid cardType: can't find "${cardType}" at at the expected paths "${cardTypePath}" or "${customCardTypePath}" for vertical "${verticalName}".`);
+    return false;
   }
   return true;
 }
@@ -101,15 +103,16 @@ function isCardTypeValid(verticalName, themeDir, cardType) {
  * 
  * @param {Object} verticalConfigs
  * @param {Object} jamboConfig
+ * @param {Object<string, Function|string>} partials mapping of partial name to partial
  * @returns {boolean}
  */
-function isAllVerticalConfigsValid(verticalConfigs, jamboConfig) {
+function isAllVerticalConfigsValid(verticalConfigs, jamboConfig, partials) {
   if (!verticalConfigs) {
     return true;
   }
   return Object.keys(verticalConfigs)
     .map(key => {
-      return isPageVerticalConfigValid(verticalConfigs[key], jamboConfig);
+      return isPageVerticalConfigValid(verticalConfigs[key], jamboConfig, partials);
     })
     .every(result => result);
 }
