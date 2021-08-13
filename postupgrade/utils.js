35b3ec8c819168eed0ac6bd6a9996ca975df9fea
Oliver Shi <oshi@yext.com>
2021-08-12 21:59:05 -0400

    remove duplicated global config commented out props on upgrade
    This has been an issue with our global config merging for a long time.
    The way comment-json works is that each comment is associated with a
    specific "prop" of the json (unless the json is empty or the comment
    is outside of the main json body).
    
    If we're not careful with how we add new config to the global_config,
    the prop associated with the giant commented out config block will change.
    Because we merge the new theme's global_config with the user's current
    global_config on upgrade (with the user's having higher priority), this
    results in said huge block being duplicated, because comment-json sees
    it as a *different* comment than the preexisting giant one, since it is
    now associated with a different prop.
    
    We can get around this by adding pruning code, specifically for our
    special "comment out config" pattern.
    
    J=SLAP-1514
    TEST=manual, auto
    
    unit tests
    test upgrading yanswers, also with some minor tweaks to their config
    to test things

--- postupgrade/utils.js
+++ postupgrade/utils.js
@@ -5,21 +5,83 @@ const simpleGit = require('simple-git/promise')();
 
 /**
  * Merges two comment-json files, with the original having higher priority.
- * @param {string} updatedCommentJson 
- * @param {string} originalCommentJson 
+ * Prunes away duplicated commented out props.
+ *
+ * @param {string} newJson 
+ * @param {string} originalJson 
  * @returns {string}
  */
-exports.mergeJson = function(updatedCommentJson, originalCommentJson) {
-  const merged = assign(parse(updatedCommentJson), parse(originalCommentJson));
+exports.mergeGlobalConfigs = function (newJson, originalJson) {
+  const originalParsed = parse(originalJson);
+  const commentsFromOriginal = parseCommentedOutConfig(originalParsed);
+  const newPruned = pruneDuplicatedComments(parse(newJson), commentsFromOriginal);
+  const merged = assign(newPruned, originalParsed);
   return stringify(merged, null, 2);
 }
+
+/**
+ * Parses "commented out" config values.
+ *
+ * @param {import('comment-json').CommentJSONValue} jsonWithComments
+ * @returns {import('comment-json').CommentToken[]}
+ */
+function parseCommentedOutConfig(commentJsonValue) {
+  return getPropCommentSymbols(commentJsonValue).flatMap(symbol => {
+    const commentArr = commentJsonValue[symbol] || [];
+    // We only care about non-inline LineComments that "look like" a config option
+    return commentArr.filter(c => {
+      return !c.inline && c.type === 'LineComment' && c.value.match(/^\s?"\w+":/)
+    })
+  });
+}
+exports.parseCommentedOutConfig = parseCommentedOutConfig;
+
+/**
+ * Removes all comments in CommentJSONValue that have the same value and type as the given
+ * commentsToDedupe.
+ *
+ * @param {import('comment-json').CommentJSONValue} commentJsonValue
+ * @param {import('comment-json').CommentToken[]} commentsToDedupe
+ * @returns {import('comment-json').CommentJSONValue} the updated value
+ */
+function pruneDuplicatedComments(commentJsonValue, commentsToDedupe) {
+  return getPropCommentSymbols(commentJsonValue).reduce((prunedJson, symbol) => {
+    if (!commentJsonValue[symbol]) {
+      return prunedJson;
+    }
+    prunedJson[symbol] = commentJsonValue[symbol].filter(commentToCheck => {
+      return !commentsToDedupe.find(c => {
+        return c.value === commentToCheck.value
+          && c.type === commentToCheck.type
+          && c.inline === commentToCheck.inline;
+      })
+    });
+    return prunedJson;
+  }, { ...commentJsonValue });
+}
+exports.pruneDuplicatedComments = pruneDuplicatedComments
+
+/**
+ * Returns an array of global symbols for before, before:[prop], and after:[prop] comments.
+ * These are the comment types that can be used for commented out config props.
+ * 
+ * @param {import('comment-json').CommentJSONValue} commentJsonValue
+ * @returns {symbol[]}
+ */
+function getPropCommentSymbols(commentJsonValue) {
+  const beforeAndAfterSymbols = Object.keys(commentJsonValue).flatMap(prop => {
+    return [Symbol.for(`before:${prop}`), Symbol.for(`after:${prop}`)]
+  });
+  return beforeAndAfterSymbols.concat(Symbol.for('before'));
+}
+exports.getPropCommentSymbols = getPropCommentSymbols;
 
 /**
  * Gets the value of a given --jambo command line argument.
  * @param {string} param 
  * @returns {string}
  */
-exports.getJamboParam = function(param) {
+exports.getJamboParam = function (param) {
   const getProcessArgument = (param) => {
     const nameIndex = process.argv.indexOf(param);
     if (nameIndex === -1) {
--- tests/postupgrade/utils.js
+++ tests/postupgrade/utils.js
@@ -1,17 +1,79 @@
-const { mergeJson } = require('postupgrade/utils.js');
+const { parse, stringify } = require('comment-json');
 const fs = require('fs');
+const {
+  parseCommentedOutConfig,
+  mergeGlobalConfigs,
+  getPropCommentSymbols,
+  pruneDuplicatedComments
+} = require('../../postupgrade/utils');
 
-describe('postupgrade utils', () => {
-  describe('mergeJson', () => {
-    it('puts together two comment JSON strings', () => {
-      const config1 = fs.readFileSync('./tests/postupgrade/config1.json', 'utf-8');
-      const config2 = fs.readFileSync('./tests/postupgrade/config2.json', 'utf-8');
-      const expected = fs.readFileSync('./tests/postupgrade/config3.json', 'utf-8');
+it('mergeGlobalConfigs merges together two global configs, and removes duplicated comments', () => {
+  const originalConfig = fs.readFileSync('./tests/postupgrade/original.json', 'utf-8');
+  const newConfig = fs.readFileSync('./tests/postupgrade/new.json', 'utf-8');
+  const expected = fs.readFileSync('./tests/postupgrade/merged.json', 'utf-8');
+  const mergedConfig = mergeGlobalConfigs(newConfig, originalConfig);
+  expect(mergedConfig).toEqual(expected);
+});
 
-      // if this passes, then it is correctly not deleting any fields or comments
-      // and it is giving precedence to the second parameter config
-      const mergedConfig = mergeJson(config1, config2);
-      expect(mergedConfig).toEqual(expected);
-    });
-  });
+it('getPropCommentSymbols ', () => {
+  const testJson = parse(`{
+    // "sdkVersion": "1.9", // sdkVersion comment
+    "experienceVersion": "<REPLACE ME>", // experienceVersion comment
+    "businessId": "<REPLACE ME>", // businessId comment
+    // "initializeManually": true, // manualInit comment
+  }`);
+  const commentSymbols = getPropCommentSymbols(testJson);
+  expect(commentSymbols).toEqual([
+    Symbol.for('before:experienceVersion'),
+    Symbol.for('after:experienceVersion'),
+    Symbol.for('before:businessId'),
+    Symbol.for('after:businessId'),
+    Symbol.for('before')
+  ]);
+});
+
+it('parseCommentedOutConfig parses correctly', () => {
+  const commentJson = parse(`{
+    // "sdkVersion": "1.9", // sdkVersion comment
+    "experienceVersion": "<REPLACE ME>", // experienceVersion comment
+    "businessId": "<REPLACE ME>", // businessId comment
+    // "initializeManually": true, // manualInit comment
+  }`);
+  const commentTokens = parseCommentedOutConfig(commentJson);
+  expect(commentTokens).toMatchObject([
+    {
+      'inline': false,
+      'type': 'LineComment',
+      'value': ' "sdkVersion": "1.9", // sdkVersion comment'
+    },
+    {
+      'inline': false,
+      'type': 'LineComment',
+      'value': ' "initializeManually": true, // manualInit comment'
+    }
+  ]);
+});
+
+it('pruneDuplicatedComments prunes all comments that match value, type, and inlining', () => {
+  const jsonToPrune = parse(`{
+    //a comment
+    "experienceVersion": "<REPLACE ME>", //a comment
+    /*a comment*/
+    "businessId": "<REPLACE ME>" //a comment
+    //a comment
+  }`);
+  const commentsToDedupe = [
+    {
+      'inline': false,
+      'type': 'LineComment',
+      'value': 'a comment'
+    }
+  ];
+  const prunedJson = stringify(pruneDuplicatedComments(jsonToPrune, commentsToDedupe), null, 2);
+  const expected = stringify(parse(`{
+    "experienceVersion": "<REPLACE ME>", //a comment
+    /*a comment*/
+    "businessId": "<REPLACE ME>" //a comment
+  }`), null, 2);
+  expect(prunedJson).toEqual(expected);
 });
