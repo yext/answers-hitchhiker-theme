436e2253b76dfec0a0af3dabf7b7f2100e88199e
Yen Truong <ytruong@yext.com>
2021-10-08 15:58:38 -0400

    parallel percy jobs

--- tests/percy/index.js
+++ tests/percy/index.js
@@ -9,34 +9,56 @@ const puppeteer = require('puppeteer');
 const percySnapshot = require('@percy/puppeteer');
 const PORT = 5042;
 
-(async ()=> {
+async function defaultSnapshots(page) {
+  const standardPageNavigator = new StandardPageNavigator(page, `http://localhost:${PORT}`);
+  const standardCamera = new Camera(percySnapshot, page);
+  await (new Photographer(standardPageNavigator, standardCamera).captureSnapshots());
+}
+
+async function iframeSnapshots(page) {
+  const iframePageNavigator = new IframePageNavigator(page, `http://localhost:${PORT}`, 'iframe_test');
+  const iframeCamera = new Camera(percySnapshot, page, true);
+  await (new Photographer(iframePageNavigator, iframeCamera).captureSnapshots());
+}
+
+async function nonEnglishSnapshots(page) {
+  const standardPageNavigator = new StandardPageNavigator(page, `http://localhost:${PORT}`);
+  const standardCamera = new Camera(percySnapshot, page);
+  standardPageNavigator.setCurrentLocale('es');
+  standardCamera.setLocale('es');
+  await (new MultilangPhotographer(standardPageNavigator, standardCamera, queryConfig.es).captureSnapshots());
+}
+
+async function RtlSnapshots(page) {
+  const standardPageNavigator = new StandardPageNavigator(page, `http://localhost:${PORT}`);
+  const standardCamera = new Camera(percySnapshot, page);
+  standardPageNavigator.setCurrentLocale('ar');
+  standardCamera.setLocale('ar');
+  await (new MultilangPhotographer(standardPageNavigator, standardCamera, queryConfig.ar).captureSnapshots());
+}
+
+async function runPercyTest() {
   const server = new HttpServer({
     dir: 'test-site/public',
     port: PORT
   });
   server.start();
-
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const standardPageNavigator = new StandardPageNavigator(page, `http://localhost:${PORT}`);
-  const iframePageNavigator = new IframePageNavigator(page, `http://localhost:${PORT}`, 'iframe_test');
-
-  const standardCamera = new Camera(percySnapshot, page);
-  const iframeCamera = new Camera(percySnapshot, page, true);
-
-  await (new Photographer(standardPageNavigator, standardCamera).captureSnapshots());
-  await (new Photographer(iframePageNavigator, iframeCamera).captureSnapshots());
-
-  standardPageNavigator.setCurrentLocale('es');
-  standardCamera.setLocale('es');
-  
-  await (new MultilangPhotographer(standardPageNavigator, standardCamera, queryConfig.es).captureSnapshots());
-
-  standardPageNavigator.setCurrentLocale('ar');
-  standardCamera.setLocale('ar');
-  await (new MultilangPhotographer(standardPageNavigator, standardCamera, queryConfig.ar).captureSnapshots());
+  const snapshotType = process.argv[2];
+  if (snapshotType === 'rtl') {
+    await RtlSnapshots(page);
+  } else if (snapshotType === 'non-english') {
+    await nonEnglishSnapshots(page);
+  }else if (snapshotType === 'iframe') {
+    await iframeSnapshots(page);
+  } else {
+    await defaultSnapshots(page);
+  }
 
   await browser.close();
   server.shutdown();
-})();
+};
+
+runPercyTest();
