55ef90d5eeb9a5d08df79705ad872cd4c1eeeb19
Tom Meyer <tmeyer@yext.com>
2021-12-13 13:58:55 -0500

    Integrate helper.

--- hbshelpers/getDefaultUniversalLimit.js
+++ hbshelpers/getDefaultUniversalLimit.js
@@ -1,9 +1,23 @@
-module.exports = function getDefaultUniversalLimit(verticalsToConfig) {
-  return Object.entries().reduce(([key, config], limit) => {
-    if (config.universalLimit) {
-      limit[key] = config.universalLimit;
-    }
+/**
+ * Generates a default result limit for each vertical on the universal page. The
+ * default is taken from the vertical's `universalLimit`, specified in the related
+ * page's VTC. If there is no such `universalLimit`, the back-end default of 10 will
+ * be used.
+ * 
+ * @param {Object} pageConfigs - The configurations for each page.
+ * @returns {Object} The default search.universalLimit for the page.
+ */
+ module.exports = function getDefaultUniversalLimit(pageConfigs) {
+  const universalLimit = Object.entries(pageConfigs)
+    .filter(([key, _]) => key != 'index')
+    .reduce((limit, [key, config]) => {
+      const verticalKey = config.verticalKey;
+      if (config.verticalsToConfig?.[verticalKey]?.universalLimit) {
+        limit[verticalKey] = config.verticalsToConfig[verticalKey].universalLimit;
+      }
 
-    return limit;
-  }, {});
+      return limit;
+    }, {});
+
+    return { universalLimit };
 }
