5fc1d3fea5659e4ceabcc67a0e2c83342a56fadd
Connor Anderson <canderson@yext.com>
2021-12-20 19:37:21 -0500

    use runner

--- .github/run_browserstack_acceptance.sh
+++ .github/run_browserstack_acceptance.sh
@@ -11,14 +11,4 @@ export BROWSERSTACK_CONSOLE="verbose"
 export BROWSERSTACK_NETWORK_LOGS="true"
 export BROWSERSTACK_DISPLAY_RESOLUTION="1024x768"
 
-if [[ $GITHUB_BRANCH == release/*
-  || $GITHUB_BRANCH == hotfix/*
-  || $GITHUB_BRANCH == master
-  || $GITHUB_BRANCH == support/* ]]
-then
-  npm run acceptance -- --browsers browserstack:ie@11.0 browserstack:safari browserstack:firefox
-else
-  # npm run acceptance -- --browsers browserstack:ie@11.0 --concurrency 2
-  npx serve -l tcp://0.0.0.0:9999 test-site/public &
-  npx testcafe $TEST_BROWSER tests/acceptance/suites/*
-fi
+npm run acceptance -- --browsers $TEST_BROWSWER
