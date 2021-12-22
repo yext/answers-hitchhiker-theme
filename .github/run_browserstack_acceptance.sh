3e8aa2a8d93065499f67534a6aa1f04ad19666b0
Connor Anderson <canderson@yext.com>
2021-12-21 19:52:44 -0500

    feedback

--- .github/run_browserstack_acceptance.sh
+++ .github/run_browserstack_acceptance.sh
@@ -6,14 +6,15 @@ export BROWSERSTACK_BUILD_ID="${GITHUB_BRANCH} - ${GITHUB_RUN_ID}"
 COMMIT_MSG_TITLE=$(git log -n 1 --pretty=format:%s)
 export BROWSERSTACK_TEST_RUN_NAME=$COMMIT_MSG_TITLE
 export BROWSER=$1
+export CONCURRENCY=$2
 export BROWSERSTACK_DEBUG="true"
 export BROWSERSTACK_CONSOLE="verbose"
 export BROWSERSTACK_NETWORK_LOGS="true"
 export BROWSERSTACK_DISPLAY_RESOLUTION="1920x1080"
 
-if [[ $BROWSER == 'browserstack:ie@11.0' ]]
+if [[ -n $CONCURRENCY ]] # If $CONCURRENCY is non-zero
 then
-  npm run acceptance -- --browsers $BROWSER --concurrency 3
+  npm run acceptance -- --browsers $BROWSER --concurrency $CONCURRENCY
 else
   npm run acceptance -- --browsers $BROWSER 
 fi
