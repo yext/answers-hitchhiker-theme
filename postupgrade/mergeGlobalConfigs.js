1342b8b331ea8602eacffb4f9a6f3bab8205c699
Oliver Shi <oshi@yext.com>
2022-01-05 16:29:02 -0500

    refactor global config merging strategy on upgrade
    This commit refactors how global configs are merged on theme upgrades.
    Now, global configs will always have the same "structure" as the incoming
    config, instead of sometimes placing properties in seemingly random spots,
    if the two configs don't match up exactly in terms of structure.
    
    Per Rose's request, the sdkVersion is now always updated to the incoming
    config's value. Besides that, preexisting properties will favor the
    original config, while commented out properties will favor the incoming config.
    
    Properties and comments in the original that do no exist in the incoming config
    will be moved to the bottom of the config.
    
    J=SLAP-1717
    TEST=manual,auto
    
    ran an upgrade

---
+++ postupgrade/mergeGlobalConfigs.js
@@ -0,0 +1,174 @@
+const { parse, stringify } = require('comment-json');
+
+/**
+ * @typedef {import('comment-json').CommentJSONObject} CommentJSONObject
+ * @typedef {import('comment-json').CommentToken} CommentToken
+ * 
+ * @typedef {{
+ *   type: 'CommentedOutProperty',
+ *   key: string,
+ *   comment: Omit<CommentToken, 'loc'>
+ * }} CommentedOutPropertyToken
+ * 
+ * @typedef {{
+ *   type: 'Property',
+ *   key: string,
+ *   value: string | number | null
+ *   inlineComment?: Omit<CommentToken, 'loc'>
+ * }} PropertyToken
+ * 
+ * @typedef {PropertyToken | CommentedOutPropertyToken} GlobalConfigToken
+ */
+
+/**
+ * Merges two global configs.
+ * For regular properties, the original config has priority.
+ * For commented out properties, the incoming config has priority.
+ * The `sdkVersion` is a special property which will always favor the incoming config's value,
+ * but the original's inline comment (if one exists).
+ *
+ * This is not intended to handle json with duplicate keys, i.e. a json config
+ * with an `apiKey` property in addition to a commented out `apiKey` property.
+ * 
+ * @param {string} originalConfig 
+ * @param {string} incomingConfig 
+ * @return {string}
+ */
+exports.mergeGlobalConfigs = function mergeGlobalConfigs(originalConfig, incomingConfig) {
+  const originalJson = parse(originalConfig);
+  const incomingJson = parse(incomingConfig);
+  const originalTokens = tokenize(originalJson);
+  const incomingTokens = tokenize(incomingJson);
+  const mergedTokens = mergeTokens(originalTokens, incomingTokens);
+  const commentJsonObject = transformToCommentJsonObject(mergedTokens);
+  if (incomingJson.sdkVersion) {
+    commentJsonObject.sdkVersion = incomingJson.sdkVersion
+  }
+  return stringify(commentJsonObject, null, 2);
+}
+
+/**
+ * Parses a {@link CommentJSONObject} into an array of {@link GlobalConfigToken}s
+ * 
+ * @param {CommentJSONObject}  
+ * @returns {GlobalConfigToken[]}
+ */
+function tokenize(commentJsonObject) {
+  /** @type {GlobalConfigToken[]} */
+  const tokens = []
+
+  /**
+   * @param {CommentToken} comment 
+   * @returns {CommentedOutPropertyToken}
+   */
+  const parseCommentedOutProperty = (comment) => {
+    const propCommentRegex = /^\s?"([^"]+)":.*$/g
+    const val = comment.value
+    const match = [...val.matchAll(propCommentRegex)][0]
+    if (!match || match.length !== 2) {
+      throw new Error(`Could not parse global config commented out property: \`${val}\``)
+    }
+    return {
+      type: 'CommentedOutProperty',
+      key: match[1],
+      comment: {
+        type: comment.type,
+        value: comment.value,
+        inline: comment.inline
+      }
+    }
+  }
+
+  const handleComment = c => {
+    if (c.inline) return;
+    tokens.push(parseCommentedOutProperty(c));
+  }
+
+  Object.keys(commentJsonObject).forEach(key => {
+    const beforeSymbol = Symbol.for(`before:${key}`)
+    const afterSymbol = Symbol.for(`after:${key}`)
+    const beforeComments = commentJsonObject[beforeSymbol] || []
+    const afterComments = commentJsonObject[afterSymbol] || []
+    beforeComments.forEach(handleComment)
+    const inlineComment = afterComments.find(c => c.inline);
+    tokens.push({
+      type: 'Property',
+      key,
+      value: commentJsonObject[key],
+      inlineComment: inlineComment ? {
+        type: inlineComment.type,
+        value: inlineComment.value,
+        inline: inlineComment.inline
+      } : null
+    })
+    afterComments.forEach(handleComment)
+  })
+
+  return tokens;
+}
+exports.tokenize = tokenize;
+
+/**
+ * Transforms an array of {@link GlobalConfigToken} into an equivalent {@link CommentJSONObject}
+ * 
+ * @param {GlobalConfigToken[]} tokens 
+ * @returns {CommentJSONObject}
+ */
+function transformToCommentJsonObject(tokens) {
+  const commentJsonObject = {}
+  let previousPropertyKey = null;
+  tokens.forEach(token => {
+    switch (token.type) {
+      case 'Property':
+        commentJsonObject[token.key] = token.value
+        previousPropertyKey = token.key
+        if (token.inlineComment) {
+          const symbol = Symbol.for(`after:${token.key}`);
+          commentJsonObject[symbol] = (commentJsonObject[symbol] || []).concat([token.inlineComment])
+        }
+        break;
+      case 'CommentedOutProperty':
+        const symbol = previousPropertyKey ? Symbol.for(`after:${previousPropertyKey}`) : Symbol.for('before');
+        commentJsonObject[symbol] = (commentJsonObject[symbol] || []).concat([token.comment])
+        break;
+      default:
+        throw new Error(`Unknown token type "${token.type}"`);
+    }
+  });
+  return commentJsonObject;
+}
+exports.transformToCommentJsonObject = transformToCommentJsonObject;
+
+/**
+ * The logic for merging two arrays of {@link GlobalConfigToken}.
+ * 
+ * @param {GlobalConfigToken[]} original 
+ * @param {GlobalConfigToken[]} incoming 
+ * @returns 
+ */
+ function mergeTokens(original, incoming) {
+  const mergedTokens = []
+  const originalCopy = [...original]
+
+  incoming.forEach(incomingToken => {
+    const i = originalCopy.findIndex(t => t.key === incomingToken.key)
+    if (i < 0) {
+      mergedTokens.push(incomingToken);
+      return;
+    }
+    const originalToken = originalCopy[i]
+    if (originalToken.type === 'Property') {
+      mergedTokens.push(originalToken)
+    } else if (originalToken.type === 'CommentedOutProperty') {
+      mergedTokens.push(incomingToken);
+    }
+    originalCopy.splice(i, 1);
+  });
+
+  // Append any leftover tokens from the original,
+  // i.e tokens with keys that weren't present in the incoming tokens,
+  // or for duplicate keys (which we don't handle)
+  mergedTokens.push(...originalCopy);
+  return mergedTokens;
+}
+exports.mergeTokens = mergeTokens;
