2a49e70c038824337db4fbb918bd0d6c61c21e94
canderson <canderson@yext.com>
2022-02-18 22:55:36 +0000

    move to on-ready

--- script/on-ready.js
+++ script/on-ready.js
@@ -1,1 +1,4 @@
 /* Code added here will be executed during the Answers onReady callback */
+const context = (new URLSearchParams(window.location.search)).get('context');
+const querySource = JSON.parse(context ?? '{}').querySource;
+querySource && ANSWERS.setQuerySource(querySource);
