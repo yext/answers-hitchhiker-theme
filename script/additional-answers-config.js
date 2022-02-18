151d306a019dfc98c5fa80d645600a874268fdea
Connor Anderson <canderson@yext.com>
2022-02-18 18:17:38 -0500

    Revert "test additional answers config"
    This reverts commit 25885ec86f05b04af410e7b0bd68ac81fc434a3a.

--- script/additional-answers-config.js
+++ script/additional-answers-config.js
@@ -1,13 +1,4 @@
 // This file expects a JS object literal. 
 {
-  querySource: (() => {
-    const context = (new URLSearchParams(window.location.search)).get('context');
-    try {
-      return JSON.parse(context ?? '{}').querySource;
-    } catch (err) {
-      console.warn('Error parsing querySource from context');
-      console.warn(err)
-      return 'STANDARD';
-    }
-  })()
+
 }
