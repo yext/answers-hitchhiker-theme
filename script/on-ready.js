3f1685c831cec4385179d4d8492957aa60c75022
Connor Anderson <canderson@yext.com>
2022-02-18 18:05:47 -0500

    Revert "move to on-ready"
    This reverts commit 2a49e70c038824337db4fbb918bd0d6c61c21e94.

--- script/on-ready.js
+++ script/on-ready.js
@@ -1,4 +1,1 @@
 /* Code added here will be executed during the Answers onReady callback */
-const context = (new URLSearchParams(window.location.search)).get('context');
-const querySource = JSON.parse(context ?? '{}').querySource;
-querySource && ANSWERS.setQuerySource(querySource);
