747f1ebc0584b8fd93836b2ff5b296c358cbc455
nmanu1 <nmanu@yext.com>
2022-03-08 16:58:08 -0500

    Update doc

--- static/js/theme-map/VerticalFullPageMapOrchestrator.js
+++ static/js/theme-map/VerticalFullPageMapOrchestrator.js
@@ -484,7 +484,7 @@ class VerticalFullPageMapOrchestrator extends ANSWERS.Component {
   /**
    * The callback when a result pin on the map is clicked or tabbed onto
    * @param {Number} index The index of the pin in the current result list order
-   * @param {string} cardId The unique id for the pin entity, usually of the form `js-yl-${meta.id}`
+   * @param {string} cardId The unique id for the pin entity, usually of the form `js-yl-${uid}`
    */
   pinFocusListener (index, cardId) {
     this.core.storage.set(StorageKeys.LOCATOR_SELECTED_RESULT, cardId);
