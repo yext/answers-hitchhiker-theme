75c6731893ca9e70ee002e0cf9240a4381ed6355
Oliver Shi <oshi@yext.com>
2022-03-17 20:23:31 -0400

    pass Client-SDK HTTP header (#1056)
    This will only work when pointed to the next version of the SDK.
    
    J=SLAP-1949
    TEST=manual,auto
    
    see that the test-site specifies the theme's version in the header
    create a fresh jambo site using the hitchhiker theme, see the header passed as expected

---
+++ tests/acceptance/suites/client-sdk-http-header.js
@@ -0,0 +1,56 @@
+import {
+  PORT,
+  UNIVERSAL_AUTOCOMPLETE_URL_REGEX,
+  UNIVERSAL_SEARCH_URL_REGEX,
+  VERTICAL_SEARCH_URL_REGEX,
+  VERTICAL_AUTOCOMPLETE_URL_REGEX
+} from '../constants';
+import { SearchRequestLogger } from '../searchrequestlogger';
+import { registerIE11NoCacheHook } from '../../test-utils/testcafe';
+import packageJson from '../../../package.json';
+
+const verticalSearchLogger = (new SearchRequestLogger()).createVerticalSearchLogger();
+const verticalAutocompleteLogger = (new SearchRequestLogger()).createVerticalAutocompleteLogger();
+
+fixture`Client-SDK header works on vertical searches/autocomplete`
+  .page(`http://localhost:${PORT}/people`)
+  .requestHooks(verticalSearchLogger, verticalAutocompleteLogger)
+  .beforeEach(async t => {
+    await registerIE11NoCacheHook(t, VERTICAL_SEARCH_URL_REGEX);
+    await registerIE11NoCacheHook(t, VERTICAL_AUTOCOMPLETE_URL_REGEX);
+    await t.resizeWindow(1600, 900);
+  })
+
+test('the Client-SDK header is sent correctly on vertical search/autocomplete requests', async t => {
+  await t.expect(verticalAutocompleteLogger.requests.length).eql(1);
+  await t.expect(verticalSearchLogger.requests.length).eql(1);
+  await t
+    .expect(verticalAutocompleteLogger.requests[0].request.headers['client-sdk'])
+    .contains(`ANSWERS_THEME=${packageJson.version}`);
+  await t
+    .expect(verticalSearchLogger.requests[0].request.headers['client-sdk'])
+    .contains(`ANSWERS_THEME=${packageJson.version}`);
+});
+
+const universalSearchLogger = (new SearchRequestLogger()).createUniversalSearchLogger();
+const universalAutocompleteLogger = (new SearchRequestLogger()).createUniversalAutocompleteLogger();
+
+fixture`Client-SDK header works on universal searches/autocomplete`
+  .page(`http://localhost:${PORT}`)
+  .requestHooks(universalSearchLogger, universalAutocompleteLogger)
+  .beforeEach(async t => {
+    await registerIE11NoCacheHook(t, UNIVERSAL_SEARCH_URL_REGEX);
+    await registerIE11NoCacheHook(t, UNIVERSAL_AUTOCOMPLETE_URL_REGEX);
+    await t.resizeWindow(1600, 900);
+  })
+
+test('the Client-SDK header is sent correctly on universal search/autocomplete requests', async t => {
+  await t.expect(universalSearchLogger.requests.length).eql(1);
+  await t.expect(universalAutocompleteLogger.requests.length).eql(1);
+  await t
+    .expect(universalAutocompleteLogger.requests[0].request.headers['client-sdk'])
+    .contains(`ANSWERS_THEME=${packageJson.version}`);
+  await t
+    .expect(universalSearchLogger.requests[0].request.headers['client-sdk'])
+    .contains(`ANSWERS_THEME=${packageJson.version}`);
+});
