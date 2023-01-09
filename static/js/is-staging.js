1ca1b84be16f879ede5bd39118967b93057dae4b
Tom Meyer <tmeyer@yext.com>
2023-01-09 12:12:11 -0500

    Correct bug in `isStaging` method.
    This PR corrects a bug in the `isStaging` method. If you were referred to a Production
    site from a Staging link, `isStaging` would incorrectly report `true`. This is because
    the `includes` check on the `href` would catch the Staging `referrerPageUrl`. Simply
    comparring `window.location.hostname` instead of `window.location.href` fixes this.
    I also did some code cleanup in this method and its test suite.
    
    J=SLAP-2546
    TEST=auto

--- static/js/is-staging.js
+++ static/js/is-staging.js
@@ -1,9 +1,19 @@
+import escapeStringRegexp from 'escape-string-regexp';
+
 /**
  * Determines whether the current url is staging or production.
  */
 export function isStaging(stagingDomains) {
   const defaultStagingDomains = ['127.0.0.1', 'localhost', 'office.yext.com'];
-  const _stagingDomains = defaultStagingDomains.concat(stagingDomains);
-  const currentUrl = window.location.href;
-  return _stagingDomains.some(domain => currentUrl.includes(domain));
+  const _stagingDomains = stagingDomains
+    ? defaultStagingDomains.concat(stagingDomains)
+    : defaultStagingDomains;
+  const currentDomain = window.location.hostname;
+
+  return _stagingDomains.some(domain => {
+    const regexEscapedDomain = escapeStringRegexp(domain);
+    const isSubdomainRegex = new RegExp(`[A-Za-z0-9]?[A-Za-z0-9\-]{0,61}[A-Za-z0-9]?\.${regexEscapedDomain}`);
+
+    return domain === currentDomain || isSubdomainRegex.test(currentDomain);
+  });
 }
--- tests/static/js/is-staging.js
+++ tests/static/js/is-staging.js
@@ -6,32 +6,42 @@ describe('isStaging', () => {
   global.window = Object.create(window);
   global.window.location = {};
 
-  function setHref(href) {
-    window.location.href = href;
+  function setDomain(url) {
+    const domainRegex = /:\/\/([^:\/]+)[:\/]?/;
+    window.location.hostname = url.match(domainRegex)[1];
   }
 
   const testStagingDomains = ["yextpages.net", "landingpagespreview.com"];
-  beforeEach(() => {
-    // Reset window.location.href to the default value in a jest test
-    setHref('http://localhost/');
-  });
 
-  it('works correctly for default staging domains', () => {
+  it('handles locally hosted sites properly', () => {
+    setDomain('http://localhost:3000/my-local/url');
     expect(isStaging()).toBeTruthy();
-    setHref('http://localhost:3000/my-local/url');
-    expect(isStaging()).toBeTruthy();
-    setHref('http://127.0.0.1/my-local/url');
+    setDomain('http://127.0.0.1/my-local/url');
     expect(isStaging()).toBeTruthy()
-    setHref('http://breed.office.yext.com/my-local/url');
-    expect(isStaging()).toBeTruthy();
   });
 
-  it('can check stagingDomains correctly', () => {
-    setHref('https://yextpages.net/angelas-adventure-articles');
-    expect(isStaging()).toBeFalsy();
+  it('handles Office-hosted sites properly', () => {
+    setDomain('http://breed.office.yext.com/my-local/url');
+    expect(isStaging()).toBeTruthy();
+  })
+
+  it('handles a page of a Staging site properly', () => {
+    setDomain('https://yextpages.net/angelas-adventure-articles');
     expect(isStaging(testStagingDomains)).toBeTruthy();
-    setHref('https://bob.landingpagespreview.com/angelas-adventure-articles');
-    expect(isStaging()).toBeFalsy();
+  })
+
+  it('handles a subdomain of a Staging site properly', () => {
+    setDomain('https://bob.landingpagespreview.com/angelas-adventure-articles');
     expect(isStaging(testStagingDomains)).toBeTruthy();
   });
+
+  it('handles a port of a Staging site properly', () => {
+    setDomain('https://yextpages.net/angelas-adventure-articles:9000');
+    expect(isStaging(testStagingDomains)).toBeTruthy();
+  });
+
+  it('handles redirect from a Staging site to a Production site correctly', () => {
+    setDomain('https://production-url.com/page?referrerPageUrl=https://yextpages.net/');
+    expect(isStaging(testStagingDomains)).toBeFalsy();
+  })
 });
