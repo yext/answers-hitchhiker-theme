eb9515b1af233dca11f026c1d493d22f1e83688a
cea2aj <42848445+cea2aj@users.noreply.github.com>
2023-05-09 13:48:07 -0400

    Support EU Cloud Region (#1134)
    Add a cloud region config option to the global_config to allow either 'us' (default), or 'eu' to be set as the cloud region.
    
    The cloud region determines where the search assets and ytag are sourced from.
    
    J=BACK-2277
    TEST=manual, auto
    
    Using the test site, set the cloudRegion to "eu" and confirmed the assets are loaded from the EU CDN and executed searches against the EU search endpoints

--- hbshelpers/sdkAssetUrl.js
+++ hbshelpers/sdkAssetUrl.js
@@ -2,17 +2,25 @@ const RELEASE_BRANCH_REGEX = /^release\/v[0-9.]+$/;
 const HOTFIX_BRANCH_REGEX = /^hotfix\/v[0-9.]+$/;
 const I18N_FEATURE_BRANCH_REGEX = /^feature\/.+-i18n$/;
 const SEM_VER_REGEX = /^[1-9]+$|^[1-9]+\.[0-9]+$|^[1-9]+\.[0-9]+\.[0-9]+$/;
+const US = 'us';
+const EU = 'eu';
 
 /**
  * Given a branch (or release) of the SDK and a locale, this helper provides the correct
  * URL in the CDN for the asset.
- * 
+ *
  * @param {string} branch The branch (or release) of the SDK.
  * @param {string} locale The locale to use.
  * @param {string} assetName The name of the desired asset.
+ * @param {string} cloudRegion The cloud region to use.
  * @returns {string} The CDN URL of the localized asset.
  */
-module.exports = function sdkAssetUrl(branch, locale, assetName) {
+module.exports = function sdkAssetUrl(
+  branch,
+  locale,
+  assetName,
+  cloudRegion = US
+) {
   const isReleasedBranch = SEM_VER_REGEX.test(branch);
 
   let parsedBranch;
@@ -24,17 +32,21 @@ module.exports = function sdkAssetUrl(branch, locale, assetName) {
     parsedBranch = `dev/${branch.replace(/\//g, '-')}`;
   }
 
-  const isPreReleaseBranch = 
+  const isPreReleaseBranch =
     RELEASE_BRANCH_REGEX.test(branch) || HOTFIX_BRANCH_REGEX.test(branch);
   const isI18nFeatureBranch = I18N_FEATURE_BRANCH_REGEX.test(branch);
   const isDevelopBranch = branch === 'develop';
-  const isLocalizationSupported = 
-    (isReleasedBranch || isPreReleaseBranch || isI18nFeatureBranch || isDevelopBranch) && 
+  const isLocalizationSupported =
+    (isReleasedBranch || isPreReleaseBranch || isI18nFeatureBranch || isDevelopBranch) &&
     !(locale.startsWith('en') || assetName === 'answers.css') ;
-  
+
   const parsedAssetName = isLocalizationSupported ?
     `${locale}-${assetName}` :
     assetName;
 
-  return `https://assets.sitescdn.net/answers/${parsedBranch}/${parsedAssetName}`;
+  const domain = cloudRegion === EU
+    ? 'assets.eu.sitescdn.net'
+    : 'assets.sitescdn.net';
+
+  return `https://${domain}/answers/${parsedBranch}/${parsedAssetName}`;
 };
--- tests/acceptance/constants.js
+++ tests/acceptance/constants.js
@@ -1,5 +1,5 @@
 exports.PORT=9999;
-exports.VERTICAL_SEARCH_URL_REGEX=/v2\/accounts\/me\/answers\/vertical\/query/;
-exports.UNIVERSAL_SEARCH_URL_REGEX=/v2\/accounts\/me\/answers\/query/;
-exports.UNIVERSAL_AUTOCOMPLETE_URL_REGEX=/v2\/accounts\/me\/answers\/autocomplete/;
-exports.VERTICAL_AUTOCOMPLETE_URL_REGEX=/v2\/accounts\/me\/answers\/vertical\/autocomplete/;
+exports.VERTICAL_SEARCH_URL_REGEX=/v2\/accounts\/me\/search\/vertical\/query/;
+exports.UNIVERSAL_SEARCH_URL_REGEX=/v2\/accounts\/me\/search\/query/;
+exports.UNIVERSAL_AUTOCOMPLETE_URL_REGEX=/v2\/accounts\/me\/search\/autocomplete/;
+exports.VERTICAL_AUTOCOMPLETE_URL_REGEX=/v2\/accounts\/me\/search\/vertical\/autocomplete/;
