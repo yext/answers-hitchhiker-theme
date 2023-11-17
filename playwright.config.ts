f04d28ae2549f579deecd573a5346090cccd5e29
Liah Kim <likim@yext.com>
2023-11-17 12:45:55 -0500

    Implement playwright acceptance tests for vertical full page map

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,10 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
