f13569b5d9b4e3abc5b3a765d2f8ab2d382c4d83
Liah Kim <likim@yext.com>
2023-11-17 12:45:55 -0500

    Implement playwright acceptance tests for vertical full page map

--- e2e/vertical-full-page-map.spec.ts
+++ e2e/vertical-full-page-map.spec.ts
@@ -17,7 +17,7 @@ test.describe('full page map test suite', () => {
     await page.getByPlaceholder('Search for locations').fill('virginia');
     await page.getByPlaceholder('Search for locations').press('Enter');
     await page.getByRole('button', { name: 'Result number 5' }).click();
-    const locator = await page.locator('#js-answersVerticalResults div').nth(134);
+    const locator = page.locator('#js-answersVerticalResults div').nth(134);
     await expect(locator).toHaveClass(/pinFocused/);
   });
 
@@ -37,12 +37,12 @@ test.describe('full page map test suite', () => {
     await page.getByPlaceholder('Search for locations').press('Enter');
     const responsePromise = await page.waitForResponse(/https:\/\/prod-cdn\.us\.yextapis\.com\/v2\/accounts\/me\/search\/vertical/i);
     await page.getByLabel('Map controls').getByText('Search When Map Moves').click();
-    const response = await responsePromise;
+    const response = responsePromise;
   });
 
   test('default initial search works and is enabled by default', async ({ page }) => {
     await page.getByPlaceholder('Search for locations').press('Enter');
-    const result = await page.locator('#js-answersVerticalResults div').nth(0);
+    const result = page.locator('#js-answersVerticalResults div').nth(0);
     await expect(result).toBeAttached();
   });
 
@@ -50,7 +50,7 @@ test.describe('full page map test suite', () => {
     await page.getByPlaceholder('Search for locations').fill('virginia');
     await page.getByPlaceholder('Search for locations').press('Enter');
     await page.getByLabel('Go to the next page of results').click();
-    const secondPage = await page.locator('#js-answersVerticalResultsCount');
+    const secondPage = page.locator('#js-answersVerticalResultsCount');
     await expect(secondPage).toHaveText(/21/);
   });
 
@@ -58,7 +58,7 @@ test.describe('full page map test suite', () => {
     await page.getByPlaceholder('Search for locations').fill('virginia');
     await page.getByPlaceholder('Search for locations').press('Enter');
     await page.getByLabel('Go to the next page of results').click();
-    const locator = await page.locator('#js-answersVerticalResults div').nth(0);
+    const locator = page.locator('#js-answersVerticalResults div').nth(0);
     await expect(locator).toBeVisible();
   });
 
@@ -74,7 +74,7 @@ test.describe('full page map with filters test suite', () => {
   test('clicking on a pin closes the filter view', async ({ page }) => {
     await page.getByRole('button', { name: 'filter results' }).click();
     await page.getByText('Cats (1)').click();
-    const filterView = await page.getByLabel('Main location search').locator('div').filter({ hasText: 'Filters Services reset Cats (1) Dogs (1) Sleep (1)' }).first();
+    const filterView = page.getByLabel('Main location search').locator('div').filter({ hasText: 'Filters Services reset Cats (1) Dogs (1) Sleep (1)' }).first();
     await expect(filterView).toBeVisible();
     await page.getByRole('button', { name: 'Result number 1' }).click();
     await expect(filterView).not.toBeVisible();
@@ -84,13 +84,13 @@ test.describe('full page map with filters test suite', () => {
     const mapboxPinCount = await page.locator('#js-answersMap div').count();
     await page.getByRole('button', { name: 'Cluster of 2 results' }).click();
     const mapboxPinCountAfterSelectingCluster = await page.locator('#js-answersMap div').count();
-    await expect(mapboxPinCount).not.toBe(mapboxPinCountAfterSelectingCluster);
+    expect(mapboxPinCount).not.toBe(mapboxPinCountAfterSelectingCluster);
   });
 
   test('clicking on a cluster causes a new search to be run', async ({ page }) => {
     const numResults = await page.locator('#js-answersVerticalResults').count();
     await page.getByRole('button', { name: 'Cluster of 4 results' }).click();
     const responsePromise = await page.waitForResponse(/https:\/\/prod-cdn\.us\.yextapis\.com\/v2\/accounts\/me\/search\/vertical/i);
-    const response = await responsePromise;
+    const response = responsePromise;
   });
 });
