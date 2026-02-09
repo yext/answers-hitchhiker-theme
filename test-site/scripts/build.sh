6153e9bbd22e4df0ce1919bc32ef17d4e51ae32a
Kyle Gerner <kgerner@yext.com>
2026-02-09 16:39:40 -0500

    print node modules in build step

--- test-site/scripts/build.sh
+++ test-site/scripts/build.sh
@@ -17,6 +17,13 @@ if [ ! -f ./public/index.html ]; then
   ls -la ./public | head -n 50
   exit 1
 fi
+echo "pwd before executing chmod: $(pwd)"
+if [ -d ./public/static/node_modules ]; then
+  echo "public/static/node_modules listing (first 50):"
+  ls -la ./public/static/node_modules | head -n 50
+else
+  echo "public/static/node_modules does not exist."
+fi
 chmod u+x ./public/static/node_modules/esbuild/bin/esbuild
 echo "Added execute permissions to esbuild binary."
 npx grunt webpack
